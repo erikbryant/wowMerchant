aca7db8fa799379526a318d960cd468e06e0b092
Erik Bryant <erikbryantology@gmail.com>
2025-03-27 14:27:23 -0700

    Factor, better error checking

--- Merchant/Merchant.lua
+++ Merchant/Merchant.lua
@@ -1,35 +1,50 @@
 local MerchantOpen = false
 
+-- Return the coordinates of the next slot (or false if at end of bags)
+local function nextSlot(bag, slot)
+    slot = slot + 1
+
+    if slot > C_Container.GetContainerNumSlots(bag) then
+        slot = 1
+        bag = bag + 1
+        if bag > 4 then
+            -- Ignore the reagent bag
+            return -1, -1, false
+        end
+    end
+
+    return bag, slot, true
+end
+
+-- Find the first slot after the marker item (the hearthstone)
+local function findFirstSellable()
+    local bag = 0
+    local slot = 1
+    local found = true
+
+    while found do
+        local name = C_Container.GetContainerItemLink(bag, slot)
+        if name ~= nil and string.find(name, "[Hearthstone]", 0, true) then
+            return nextSlot(bag, slot)
+        end
+        bag, slot, found = nextSlot(bag, slot)
+    end
+
+    Utility.PrettyPrint("Hearthstone not found")
+    return -1, -1, false
+end
+
+-- Sell all items after the marker and before the reagent bag
 function SellAll()
     if not MerchantOpen then
         Utility.PrettyPrint("Go find a merchant!")
         return
     end
 
-    local bag = 0
-    local slot = 1
-    local foundMarker = false
-
-    while true do
-        local name = C_Container.GetContainerItemLink(bag, slot)
-        if name ~= nil then
-            if foundMarker then
-                C_Container.UseContainerItem(bag,slot)
-            else
-                if string.find(name, "[Hearthstone]", 0, true) then
-                    foundMarker = true
-                end
-            end
-        end
-
-        slot = slot + 1
-        if slot > C_Container.GetContainerNumSlots(bag) then
-            slot = 1
-            bag = bag + 1
-            if bag > 4 then
-                break
-            end
-        end
+    local bag, slot, found = findFirstSellable()
+    while found do
+        C_Container.UseContainerItem(bag,slot)
+        bag, slot, found = nextSlot(bag, slot)
     end
 end
 
