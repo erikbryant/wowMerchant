63ab0df5e7d4f116996a6a4bd21461da2ae22646
Erik Bryant <erikbryantology@gmail.com>
2025-08-05 22:02:03 +0200

    Safer way to get all mail

--- Merchant/AutoMail.lua
+++ Merchant/AutoMail.lua
@@ -1,7 +1,17 @@
+local OpenedMail = false
+
 -- Dispatch an incoming event
 local function OnEvent(self, event)
     if event == "MAIL_INBOX_UPDATE" then
+        if OpenedMail then
+            -- If we already opened the mail, don't keep opening
+            return
+        end
+        OpenedMail = true
         OpenAllMail:OnClick()
+    end
+    if event == "MAIL_CLOSED" then
+        OpenedMail = false
     end
 end
 
@@ -9,3 +19,4 @@ local AutoMail = CreateFrame("Frame", "AutoMail", UIParent)
 AutoMail:Hide()
 AutoMail:SetScript("OnEvent", OnEvent)
 AutoMail:RegisterEvent("MAIL_INBOX_UPDATE")
+AutoMail:RegisterEvent("MAIL_CLOSED")
