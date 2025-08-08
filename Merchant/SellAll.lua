4436b9aec12f29fa66cb6559c8dceb60d3c412f6
Erik Bryant <erikbryantology@gmail.com>
2025-08-08 14:30:26 +0200

    Guarantee all items are sold
    The previous version of the code tried to walk from start to end of the
    bags, selling each item. This worked for small bags, but for large bags
    the code ran out of execution time and was terminated before all items
    were sold.
    
    Sell each bag on its own execution cycle. This should give enough time
    for each bag to be fully emptied before running out of execution time.
    
    Also, now that we are confident we can sell everything in one pass, we
    can now terminate the merchant session and dismount (usually the sales
    are done on the back of the Traveler's Tundra mount).

--- Merchant/SellAll.lua
+++ Merchant/SellAll.lua
@@ -1,23 +1,15 @@
 local MerchantOpen = false
-local BackpackSlots = {}
-local FirstBag = 0
-local FirstSlot = 1
-local MarkerFound = false
-
-local function cacheBackpackSlots()
-    for b=0,3 do
-        BackpackSlots[b] = C_Container.GetContainerNumSlots(b)
-    end
-end
+local MaxBag = 4
 
 -- Return the coordinates of the next slot (or false if at end of bags)
 local function nextSlot(bag, slot)
+    slots = C_Container.GetContainerNumSlots(bag)
     slot = slot + 1
 
-    if slot > BackpackSlots[bag] then
+    if slot > slots then
         slot = 1
         bag = bag + 1
-        if bag > #BackpackSlots then
+        if bag > MaxBag then
             return -1, -1, false
         end
     end
@@ -27,57 +19,76 @@ end
 
 -- Find the first slot after the marker item (the hearthstone)
 local function findFirstAfterMarker()
-    if MarkerFound then
-        return FirstBag, FirstSlot, MarkerFound
-    end
-
+    local bag = 0
+    local slot = 1
+    local markerFound = false
     local isSlot = true
 
     while isSlot do
-        local name = C_Container.GetContainerItemLink(FirstBag, FirstSlot)
+        local name = C_Container.GetContainerItemLink(bag, slot)
         if name ~= nil and string.find(name, "[Hearthstone]", 0, true) then
-            FirstBag, FirstSlot, MarkerFound = nextSlot(FirstBag, FirstSlot)
-            return FirstBag, FirstSlot, MarkerFound
+            return nextSlot(bag, slot)
         end
-        FirstBag, FirstSlot, isSlot = nextSlot(FirstBag, FirstSlot)
+        bag, slot, isSlot = nextSlot(bag, slot)
     end
 
     Utility.PrettyPrint("Hearthstone not found")
     return -1, -1, false
 end
 
--- Sell all backpack items after the marker
+-- Sell all items from starting slot to end of bag
+function sellOneBag(bag, slot)
+    slots = C_Container.GetContainerNumSlots(bag)
+    while slot <= slots do
+        C_Container.UseContainerItem(bag, slot)
+        slot = slot + 1
+    end
+end
+
+function sellFunc(bag, slot)
+    return function() sellOneBag(bag, slot) end
+end
+
+-- End the merchant session
+function endMerchant()
+    CloseMerchant()
+    Dismount()
+end
+
+-- Sell all bag items after the marker
 function SellAll()
     if not MerchantOpen then
         Utility.PrettyPrint("Go find a merchant!")
         return
     end
 
-    cacheBackpackSlots()
     local bag, slot, found = findFirstAfterMarker()
-
-    while found do
-        C_Container.UseContainerItem(bag, slot)
-        bag, slot, found = nextSlot(bag, slot)
+    if not found then
+        return
     end
+
+    while bag <= MaxBag do
+        C_Timer.After(0.1, sellFunc(bag, slot))
+        bag = bag + 1
+        slot = 1
+    end
+
+    C_Timer.After(2, endMerchant)
 end
 
 -- Dispatch an incoming event
 local function OnEvent(self, event)
     if event == "MERCHANT_SHOW" then
-        FirstBag = 0
-        FirstSlot = 1
-        MarkerFound = false
         MerchantOpen = true
     elseif event == "MERCHANT_CLOSED" then
         MerchantOpen = false
    end
 end
 
-local Merchant = CreateFrame("Frame", "Merchant", UIParent)
-Merchant:Hide()
-Merchant:SetScript("OnEvent", OnEvent)
-Merchant:RegisterEvent("MERCHANT_SHOW")
-Merchant:RegisterEvent("MERCHANT_CLOSED")
+local SellAllFrame = CreateFrame("Frame", "SellAll", UIParent)
+SellAllFrame:Hide()
+SellAllFrame:SetScript("OnEvent", OnEvent)
+SellAllFrame:RegisterEvent("MERCHANT_SHOW")
+SellAllFrame:RegisterEvent("MERCHANT_CLOSED")
 
 Utility.PrettyPrint("Loaded and ready to sell!")
