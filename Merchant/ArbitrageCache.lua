aaed92dc0c7991a805e5860a98994c64788a5308
Erik Bryant <erikbryantology@gmail.com>
2025-08-28 22:36:08 +0200

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,23 +1,23 @@
 local ItemIDs = {
-    223085, -- Design: Fractured Gemstone Locket
     223101, -- Pattern: Reinforced Setae Flyers
-    223102, -- Pattern: Busy Bee's Buckle
-    223086, -- Design: Insightful Blasphemite
+    223085, -- Design: Fractured Gemstone Locket
     223060, -- Technique: Patient Alchemist's Mixing Rod
+    223086, -- Design: Insightful Blasphemite
+    223096, -- Pattern: Roiling Thunderstrike Talons
     87411, -- Plans: Bloodforged Warfists
     87408, -- Plans: Unyielding Bloodplate
     224434, -- Pattern: Dawnthread Lining
-    223098, -- Pattern: Waders of the Unifying Flame
-    223096, -- Pattern: Roiling Thunderstrike Talons
+    223102, -- Pattern: Busy Bee's Buckle
+    223087, -- Design: Culminating Blasphemite
     21841, -- Netherweave Bag
-    32428, -- Heart of Darkness
-    223051, -- Plans: Artisan Skinning Knife
-    194737, -- Illusion Parchment: Spell Shield
-    49908, -- Primordial Saronite
-    223663, -- Enchant Ring - Glimmering Haste
-    197794, -- Grand Banquet of the Kalu'ak
-    154898, -- Meaty Haunch
-    154897, -- Stringy Loins
+    199241, -- Schematic: H.E.L.P.
+    72988, -- Windwool Cloth
+    158849, -- Sawtooth Jawbone
+    158822, -- Lustrous Black Feather
+    158818, -- Canine Tongue
+    158808, -- Thick Tendon
+    221909, -- Tinker: Heartseeking Health Injector
+    221904, -- Tinker: Earthen Delivery Drill
 }
 
 ArbitrageCache = {
