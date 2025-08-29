af10016a4645a44a2accc1ed7ba6bdf850980c66
Erik Bryant <erikbryantology@gmail.com>
2025-08-29 15:53:07 +0200

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -4,22 +4,22 @@ local ItemIDs = {
     223060, -- Technique: Patient Alchemist's Mixing Rod
     223102, -- Pattern: Busy Bee's Buckle
     223086, -- Design: Insightful Blasphemite
-    223096, -- Pattern: Roiling Thunderstrike Talons
+    223098, -- Pattern: Waders of the Unifying Flame
     21841, -- Netherweave Bag
+    223051, -- Plans: Artisan Skinning Knife
+    86382, -- Pattern: Touch of the Light
+    199232, -- Schematic: Bundle of Fireworks
+    223096, -- Pattern: Roiling Thunderstrike Talons
     223087, -- Design: Culminating Blasphemite
     87411, -- Plans: Bloodforged Warfists
     87408, -- Plans: Unyielding Bloodplate
-    224434, -- Pattern: Dawnthread Lining
-    199241, -- Schematic: H.E.L.P.
-    86382, -- Pattern: Touch of the Light
     72988, -- Windwool Cloth
-    223098, -- Pattern: Waders of the Unifying Flame
-    223051, -- Plans: Artisan Skinning Knife
-    199232, -- Schematic: Bundle of Fireworks
+    234418, -- Steamboil Gears
+    224434, -- Pattern: Dawnthread Lining
+    221876, -- Potion Bomb of Recovery
+    199241, -- Schematic: H.E.L.P.
     191578, -- Recipe: Transmute: Awakened Fire
     158849, -- Sawtooth Jawbone
-    158822, -- Lustrous Black Feather
-    158818, -- Canine Tongue
 }
 
 ArbitrageCache = {
