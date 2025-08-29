85f135151c68355c02eaa5624db6e62fd4ac7f70
Erik Bryant <erikbryantology@gmail.com>
2025-08-29 11:37:23 +0200

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -2,24 +2,24 @@ local ItemIDs = {
     223085, -- Design: Fractured Gemstone Locket
     223101, -- Pattern: Reinforced Setae Flyers
     223060, -- Technique: Patient Alchemist's Mixing Rod
-    223086, -- Design: Insightful Blasphemite
     223102, -- Pattern: Busy Bee's Buckle
+    223086, -- Design: Insightful Blasphemite
     223096, -- Pattern: Roiling Thunderstrike Talons
+    21841, -- Netherweave Bag
+    223087, -- Design: Culminating Blasphemite
     87411, -- Plans: Bloodforged Warfists
     87408, -- Plans: Unyielding Bloodplate
     224434, -- Pattern: Dawnthread Lining
-    21841, -- Netherweave Bag
-    223087, -- Design: Culminating Blasphemite
     199241, -- Schematic: H.E.L.P.
+    86382, -- Pattern: Touch of the Light
     72988, -- Windwool Cloth
     223098, -- Pattern: Waders of the Unifying Flame
+    223051, -- Plans: Artisan Skinning Knife
+    199232, -- Schematic: Bundle of Fireworks
+    191578, -- Recipe: Transmute: Awakened Fire
     158849, -- Sawtooth Jawbone
     158822, -- Lustrous Black Feather
     158818, -- Canine Tongue
-    158808, -- Thick Tendon
-    49908, -- Primordial Saronite
-    32428, -- Heart of Darkness
-    223663, -- Enchant Ring - Glimmering Haste
 }
 
 ArbitrageCache = {
