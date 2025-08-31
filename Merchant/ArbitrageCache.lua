fa3a92218f8192f852259d82b1fa7e85985a05d9
Erik Bryant <erikbryantology@gmail.com>
2025-08-31 05:05:55 +0200

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -2,24 +2,28 @@ local ItemIDs = {
     223085, -- Design: Fractured Gemstone Locket
     223101, -- Pattern: Reinforced Setae Flyers
     223060, -- Technique: Patient Alchemist's Mixing Rod
-    223102, -- Pattern: Busy Bee's Buckle
-    223086, -- Design: Insightful Blasphemite
     223098, -- Pattern: Waders of the Unifying Flame
-    21841, -- Netherweave Bag
-    223051, -- Plans: Artisan Skinning Knife
-    86382, -- Pattern: Touch of the Light
-    199232, -- Schematic: Bundle of Fireworks
+    223102, -- Pattern: Busy Bee's Buckle
     223096, -- Pattern: Roiling Thunderstrike Talons
-    223087, -- Design: Culminating Blasphemite
-    87411, -- Plans: Bloodforged Warfists
-    87408, -- Plans: Unyielding Bloodplate
-    72988, -- Windwool Cloth
-    234418, -- Steamboil Gears
-    224434, -- Pattern: Dawnthread Lining
-    221876, -- Potion Bomb of Recovery
-    199241, -- Schematic: H.E.L.P.
-    191578, -- Recipe: Transmute: Awakened Fire
-    158849, -- Sawtooth Jawbone
+    223086, -- Design: Insightful Blasphemite
+    223051, -- Plans: Artisan Skinning Knife
+    191580, -- Recipe: Transmute: Awakened Earth
+    226643, -- Plans: Beledar's Bulwark
+    199232, -- Schematic: Bundle of Fireworks
+    86382, -- Pattern: Touch of the Light
+    30323, -- Plans: Boots of the Protector
+    223061, -- Technique: Inscribed Rolling Pin
+    22153, -- Tome of Arcane Brilliance
+    192615, -- Fluorescent Fluid
+    86284, -- Pattern: Raven Lord's Gloves
+    60223, -- High-Powered Bolt Gun
+    30283, -- Pattern: Boots of the Long Road
+    23572, -- Primal Nether
+    234420, -- Handcrank Mounting System
+    234417, -- Handcrank Gears
+    234416, -- Steamboil Casing
+    234387, -- Steamboil Fuel Injector
+    234386, -- Handcrank Fuel Injector
 }
 
 ArbitrageCache = {
