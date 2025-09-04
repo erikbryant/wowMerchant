beb461774b5920415de3d5400dd1ea36954e4539
Erik Bryant <erikbryantology@gmail.com>
2025-09-04 12:25:08 +0100

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,29 +1,29 @@
 local ItemIDs = {
-    223085, -- Design: Fractured Gemstone Locket
     223101, -- Pattern: Reinforced Setae Flyers
+    223085, -- Design: Fractured Gemstone Locket
     223060, -- Technique: Patient Alchemist's Mixing Rod
-    223102, -- Pattern: Busy Bee's Buckle
+    223086, -- Design: Insightful Blasphemite
     223098, -- Pattern: Waders of the Unifying Flame
+    223102, -- Pattern: Busy Bee's Buckle
     223096, -- Pattern: Roiling Thunderstrike Talons
     223087, -- Design: Culminating Blasphemite
-    30283, -- Pattern: Boots of the Long Road
-    223086, -- Design: Insightful Blasphemite
     223051, -- Plans: Artisan Skinning Knife
-    86284, -- Pattern: Raven Lord's Gloves
-    4499, -- Huge Brown Sack
-    60223, -- High-Powered Bolt Gun
-    30323, -- Plans: Boots of the Protector
-    23572, -- Primal Nether
-    234420, -- Handcrank Mounting System
-    234417, -- Handcrank Gears
-    234416, -- Steamboil Casing
-    234387, -- Steamboil Fuel Injector
-    226643, -- Plans: Beledar's Bulwark
+    191580, -- Recipe: Transmute: Awakened Earth
+    223061, -- Technique: Inscribed Rolling Pin
+    87411, -- Plans: Bloodforged Warfists
+    86379, -- Pattern: Robe of Eternal Rule
+    86281, -- Pattern: Nightfire Robe
+    86280, -- Pattern: Murderer's Gloves
+    49908, -- Primordial Saronite
+    34664, -- Sunmote
+    30283, -- Pattern: Boots of the Long Road
+    238800, -- Soggy Bone Splinters
     224434, -- Pattern: Dawnthread Lining
-    221904, -- Tinker: Earthen Delivery Drill
-    220295, -- Unidentified Viscous Substance
+    223666, -- Enchant Ring - Glimmering Mastery
+    221756, -- Vial of Kaheti Oils
+    220282, -- Tuft of Whiskers
+    220267, -- Combustible Gland
     21877, -- Netherweave Cloth
-    200788, -- Mantacorn Horns
 }
 
 ArbitrageCache = {
