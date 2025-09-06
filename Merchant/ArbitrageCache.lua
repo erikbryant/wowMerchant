016b83b0b28ae9880124cb8758123343e66e6f15
Erik Bryant <erikbryantology@gmail.com>
2025-09-06 11:10:13 +0100

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,29 +1,29 @@
 local ItemIDs = {
-    223101, -- Pattern: Reinforced Setae Flyers
     223085, -- Design: Fractured Gemstone Locket
+    223101, -- Pattern: Reinforced Setae Flyers
     223060, -- Technique: Patient Alchemist's Mixing Rod
     223086, -- Design: Insightful Blasphemite
     223098, -- Pattern: Waders of the Unifying Flame
-    223102, -- Pattern: Busy Bee's Buckle
+    223061, -- Technique: Inscribed Rolling Pin
     223096, -- Pattern: Roiling Thunderstrike Talons
+    191578, -- Recipe: Transmute: Awakened Fire
     223087, -- Design: Culminating Blasphemite
     223051, -- Plans: Artisan Skinning Knife
+    223102, -- Pattern: Busy Bee's Buckle
     191580, -- Recipe: Transmute: Awakened Earth
-    223061, -- Technique: Inscribed Rolling Pin
-    87411, -- Plans: Bloodforged Warfists
-    86379, -- Pattern: Robe of Eternal Rule
-    86281, -- Pattern: Nightfire Robe
-    86280, -- Pattern: Murderer's Gloves
+    223738, -- Enchant Cloak - Whisper of Silken Speed
+    223714, -- Enchant Bracer - Whisper of Armored Leech
+    223669, -- Enchant Ring - Glimmering Versatility
+    87412, -- Plans: Chestplate of Limitless Faith
+    86297, -- Pattern: Stormbreaker Chestguard
+    67444, -- Crumbled Statue Remnants
     49908, -- Primordial Saronite
+    42253, -- Iceweb Spider Silk
+    36924, -- Sky Sapphire
     34664, -- Sunmote
-    30283, -- Pattern: Boots of the Long Road
-    238800, -- Soggy Bone Splinters
-    224434, -- Pattern: Dawnthread Lining
-    223666, -- Enchant Ring - Glimmering Mastery
-    221756, -- Vial of Kaheti Oils
-    220282, -- Tuft of Whiskers
-    220267, -- Combustible Gland
-    21877, -- Netherweave Cloth
+    234418, -- Steamboil Gears
+    234415, -- Handcrank Casing
+    234381, -- Handcrank Fuel Tank
 }
 
 ArbitrageCache = {
