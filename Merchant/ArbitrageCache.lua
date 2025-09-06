211f265ac695d00274a3b304fb2a4edd2027da48
Erik Bryant <erikbryantology@gmail.com>
2025-09-06 13:34:58 +0100

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,29 +1,30 @@
 local ItemIDs = {
-    223085, -- Design: Fractured Gemstone Locket
-    223101, -- Pattern: Reinforced Setae Flyers
-    223060, -- Technique: Patient Alchemist's Mixing Rod
-    223086, -- Design: Insightful Blasphemite
-    223098, -- Pattern: Waders of the Unifying Flame
-    223096, -- Pattern: Roiling Thunderstrike Talons
-    223061, -- Technique: Inscribed Rolling Pin
-    191578, -- Recipe: Transmute: Awakened Fire
-    223087, -- Design: Culminating Blasphemite
-    191580, -- Recipe: Transmute: Awakened Earth
-    223102, -- Pattern: Busy Bee's Buckle
-    223051, -- Plans: Artisan Skinning Knife
-    223738, -- Enchant Cloak - Whisper of Silken Speed
-    223714, -- Enchant Bracer - Whisper of Armored Leech
-    223669, -- Enchant Ring - Glimmering Versatility
-    165746, -- Magnetic Mining Pick
-    87412, -- Plans: Chestplate of Limitless Faith
-    86297, -- Pattern: Stormbreaker Chestguard
-    67444, -- Crumbled Statue Remnants
     49908, -- Primordial Saronite
-    42253, -- Iceweb Spider Silk
-    36924, -- Sky Sapphire
-    34664, -- Sunmote
-    234418, -- Steamboil Gears
-    234415, -- Handcrank Casing
+    67444, -- Crumbled Statue Remnants
+    86280, -- Pattern: Murderer's Gloves
+    86281, -- Pattern: Nightfire Robe
+    86297, -- Pattern: Stormbreaker Chestguard
+    87411, -- Plans: Bloodforged Warfists
+    87412, -- Plans: Chestplate of Limitless Faith
+    165746, -- Magnetic Mining Pick
+    191578, -- Recipe: Transmute: Awakened Fire
+    191580, -- Recipe: Transmute: Awakened Earth
+    201401, -- Iridescent Plume
+    223051, -- Plans: Artisan Skinning Knife
+    223060, -- Technique: Patient Alchemist's Mixing Rod
+    223061, -- Technique: Inscribed Rolling Pin
+    223085, -- Design: Fractured Gemstone Locket
+    223086, -- Design: Insightful Blasphemite
+    223087, -- Design: Culminating Blasphemite
+    223096, -- Pattern: Roiling Thunderstrike Talons
+    223098, -- Pattern: Waders of the Unifying Flame
+    223101, -- Pattern: Reinforced Setae Flyers
+    223102, -- Pattern: Busy Bee's Buckle
+    223663, -- Enchant Ring - Glimmering Haste
+    223669, -- Enchant Ring - Glimmering Versatility
+    223714, -- Enchant Bracer - Whisper of Armored Leech
+    223738, -- Enchant Cloak - Whisper of Silken Speed
+    226643, -- Plans: Beledar's Bulwark
 }
 
 ArbitrageCache = {
