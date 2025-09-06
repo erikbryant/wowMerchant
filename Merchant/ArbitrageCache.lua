e9aa2856ba99f660db48a9f2a4a7eeddc8502a8f
Erik Bryant <erikbryantology@gmail.com>
2025-09-06 13:11:08 +0100

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -4,16 +4,17 @@ local ItemIDs = {
     223060, -- Technique: Patient Alchemist's Mixing Rod
     223086, -- Design: Insightful Blasphemite
     223098, -- Pattern: Waders of the Unifying Flame
-    223061, -- Technique: Inscribed Rolling Pin
     223096, -- Pattern: Roiling Thunderstrike Talons
+    223061, -- Technique: Inscribed Rolling Pin
     191578, -- Recipe: Transmute: Awakened Fire
     223087, -- Design: Culminating Blasphemite
-    223051, -- Plans: Artisan Skinning Knife
-    223102, -- Pattern: Busy Bee's Buckle
     191580, -- Recipe: Transmute: Awakened Earth
+    223102, -- Pattern: Busy Bee's Buckle
+    223051, -- Plans: Artisan Skinning Knife
     223738, -- Enchant Cloak - Whisper of Silken Speed
     223714, -- Enchant Bracer - Whisper of Armored Leech
     223669, -- Enchant Ring - Glimmering Versatility
+    165746, -- Magnetic Mining Pick
     87412, -- Plans: Chestplate of Limitless Faith
     86297, -- Pattern: Stormbreaker Chestguard
     67444, -- Crumbled Statue Remnants
@@ -23,7 +24,6 @@ local ItemIDs = {
     34664, -- Sunmote
     234418, -- Steamboil Gears
     234415, -- Handcrank Casing
-    234381, -- Handcrank Fuel Tank
 }
 
 ArbitrageCache = {
