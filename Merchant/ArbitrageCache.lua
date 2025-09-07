6a54088a024c5111b3aa9f4ce53149a8ba742381
Erik Bryant <erikbryantology@gmail.com>
2025-09-07 11:56:20 +0100

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,11 +1,16 @@
 local ItemIDs = {
-    44739, -- Diamond-Cut Refractor Scope
+    23572, -- Primal Nether
+    30283, -- Pattern: Boots of the Long Road
     86280, -- Pattern: Murderer's Gloves
     86281, -- Pattern: Nightfire Robe
+    86379, -- Pattern: Robe of Eternal Rule
     87410, -- Plans: Ornate Battleplate of the Master
     87411, -- Plans: Bloodforged Warfists
+    191241, -- Draconium Sickle
+    191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
     192615, -- Fluorescent Fluid
+    194476, -- Plans: Obsidian Seared Hexsword
     201601, -- Runed Serevite Rod
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
@@ -18,12 +23,9 @@ local ItemIDs = {
     223101, -- Pattern: Reinforced Setae Flyers
     223102, -- Pattern: Busy Bee's Buckle
     223666, -- Enchant Ring - Glimmering Mastery
-    223732, -- Enchant Cloak - Whisper of Silken Leech
-    223738, -- Enchant Cloak - Whisper of Silken Speed
     224434, -- Pattern: Dawnthread Lining
-    226035, -- Vantus Rune: Nerub-ar Palace
     226643, -- Plans: Beledar's Bulwark
-    234419, -- Steamboil Mounting System
+    234418, -- Steamboil Gears
 }
 
 ArbitrageCache = {
