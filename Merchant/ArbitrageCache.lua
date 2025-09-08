11f36fce2b214f8c2fcfd57ec3f04d669f5ff63d
Erik Bryant <erikbryantology@gmail.com>
2025-09-08 14:52:13 +0100

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,10 +1,8 @@
 local ItemIDs = {
     29363, -- Mana Wyrmling
-    30283, -- Pattern: Boots of the Long Road
     33470, -- Frostweave Cloth
     86280, -- Pattern: Murderer's Gloves
     86281, -- Pattern: Nightfire Robe
-    86379, -- Pattern: Robe of Eternal Rule
     87410, -- Plans: Ornate Battleplate of the Master
     87411, -- Plans: Bloodforged Warfists
     94573, -- Direhorn Runt
@@ -12,6 +10,7 @@ local ItemIDs = {
     191241, -- Draconium Sickle
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
+    198910, -- Technique: Illusion Parchment: Shadow Orb
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
     223061, -- Technique: Inscribed Rolling Pin
@@ -22,7 +21,6 @@ local ItemIDs = {
     223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
     223102, -- Pattern: Busy Bee's Buckle
-    224424, -- Pattern: Artisan Chef's Hat
     224434, -- Pattern: Dawnthread Lining
     226643, -- Plans: Beledar's Bulwark
     234419, -- Steamboil Mounting System
