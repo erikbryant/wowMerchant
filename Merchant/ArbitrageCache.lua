5aaa85b161a0573ac02b9037448790c72bbdc263
Erik Bryant <erikbryantology@gmail.com>
2025-09-10 12:07:00 +0100

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,16 +1,19 @@
 local ItemIDs = {
     12833, -- Plans: Hammer of the Titans
+    44822, -- Albino Snake
+    48120, -- Obsidian Hatchling
     87410, -- Plans: Ornate Battleplate of the Master
     94573, -- Direhorn Runt
+    117445, -- Clefthoof Hide Satchel
     191236, -- Draconium Leatherworker's Toolset
     191242, -- Draconium Pickaxe
     191304, -- Sturdy Expedition Shovel
     191578, -- Recipe: Transmute: Awakened Fire
-    191580, -- Recipe: Transmute: Awakened Earth
     198910, -- Technique: Illusion Parchment: Shadow Orb
     222575, -- Hasty Alchemist's Mixing Rod
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
+    223061, -- Technique: Inscribed Rolling Pin
     223085, -- Design: Fractured Gemstone Locket
     223086, -- Design: Insightful Blasphemite
     223087, -- Design: Culminating Blasphemite
@@ -18,7 +21,6 @@ local ItemIDs = {
     223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
     223102, -- Pattern: Busy Bee's Buckle
-    224424, -- Pattern: Artisan Chef's Hat
     224434, -- Pattern: Dawnthread Lining
     226643, -- Plans: Beledar's Bulwark
 }
