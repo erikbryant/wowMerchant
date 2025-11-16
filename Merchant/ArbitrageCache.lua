3a8e5d970b44e4d033f5a1a0b79eaa41fb4763e4
Erik Bryant <erikbryantology@gmail.com>
2025-11-16 09:45:53 -0800

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,19 +1,20 @@
 local ItemIDs = {
-    44822, -- Albino Snake
     86279, -- Pattern: Liferuned Leather Gloves
-    86283, -- Pattern: Raiment of Blood and Bone
+    86380, -- Pattern: Imperial Silk Gloves
+    86382, -- Pattern: Touch of the Light
+    87408, -- Plans: Unyielding Bloodplate
     94573, -- Direhorn Runt
     117445, -- Clefthoof Hide Satchel
     191236, -- Draconium Leatherworker's Toolset
-    191237, -- Draconium Blacksmith's Hammer
-    191238, -- Draconium Leatherworker's Knife
+    191240, -- Draconium Skinning Knife
+    191304, -- Sturdy Expedition Shovel
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
-    201968, -- Enormous Grain of Salt
-    215119, -- Right-Handed Magnifying Glass
+    198116, -- Recipe: Salt-Baked Fishcake
+    201971, -- Threatening Note
+    221797, -- Bismuth-Fueled Samophlange
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
-    223061, -- Technique: Inscribed Rolling Pin
     223085, -- Design: Fractured Gemstone Locket
     223086, -- Design: Insightful Blasphemite
     223087, -- Design: Culminating Blasphemite
@@ -21,7 +22,6 @@ local ItemIDs = {
     223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
     223102, -- Pattern: Busy Bee's Buckle
-    224424, -- Pattern: Artisan Chef's Hat
     224434, -- Pattern: Dawnthread Lining
     226643, -- Plans: Beledar's Bulwark
 }
