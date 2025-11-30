1f4f313e3dbfc1941bc53601f1fbeb6f6687cc83
Erik Bryant <erikbryantology@gmail.com>
2025-11-30 16:39:09 -0500

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,7 +1,6 @@
 local ItemIDs = {
     86279, -- Pattern: Liferuned Leather Gloves
-    86380, -- Pattern: Imperial Silk Gloves
-    87408, -- Plans: Unyielding Bloodplate
+    86284, -- Pattern: Raven Lord's Gloves
     94573, -- Direhorn Runt
     191236, -- Draconium Leatherworker's Toolset
     191240, -- Draconium Skinning Knife
@@ -14,14 +13,15 @@ local ItemIDs = {
     221797, -- Bismuth-Fueled Samophlange
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
+    223061, -- Technique: Inscribed Rolling Pin
     223085, -- Design: Fractured Gemstone Locket
-    223086, -- Design: Insightful Blasphemite
     223087, -- Design: Culminating Blasphemite
     223096, -- Pattern: Roiling Thunderstrike Talons
     223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
     223102, -- Pattern: Busy Bee's Buckle
     224020, -- Recipe: Vicious Flask of the Wrecking Ball
+    224424, -- Pattern: Artisan Chef's Hat
     224434, -- Pattern: Dawnthread Lining
     226643, -- Plans: Beledar's Bulwark
 }
