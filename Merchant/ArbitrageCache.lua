254e35a977afda9cb6753b4332e0a3dcb7c9eb85
Erik Bryant <erikbryantology@gmail.com>
2025-12-06 11:29:00 -0800

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,15 +1,17 @@
 local ItemIDs = {
-    86279, -- Pattern: Liferuned Leather Gloves
-    86284, -- Pattern: Raven Lord's Gloves
+    21283, -- Grimoire of Corruption
+    21939, -- Fel Elemental Rod
+    29363, -- Mana Wyrmling
     87408, -- Plans: Unyielding Bloodplate
     94573, -- Direhorn Runt
     191236, -- Draconium Leatherworker's Toolset
+    191237, -- Draconium Blacksmith's Hammer
     191240, -- Draconium Skinning Knife
     191304, -- Sturdy Expedition Shovel
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
+    199239, -- Schematic: Tinker: Alarm-O-Turret
     201734, -- Technique: Cliffside Wylderdrake: Silver and Blue Armor
-    221797, -- Bismuth-Fueled Samophlange
     222573, -- Lightweight Scribe's Quill
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
@@ -23,7 +25,6 @@ local ItemIDs = {
     223102, -- Pattern: Busy Bee's Buckle
     224020, -- Recipe: Vicious Flask of the Wrecking Ball
     224424, -- Pattern: Artisan Chef's Hat
-    226643, -- Plans: Beledar's Bulwark
 }
 
 ArbitrageCache = {
