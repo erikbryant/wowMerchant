efbd6844022874aa26e1d8b7645403a3e9e4ba9d
Erik Bryant <erikbryantology@gmail.com>
2025-12-15 00:29:36 -0800

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,4 +1,5 @@
 local ItemIDs = {
+    4499, -- Huge Brown Sack
     21283, -- Grimoire of Corruption
     21939, -- Fel Elemental Rod
     29363, -- Mana Wyrmling
@@ -6,9 +7,11 @@ local ItemIDs = {
     87408, -- Plans: Unyielding Bloodplate
     94573, -- Direhorn Runt
     191237, -- Draconium Blacksmith's Hammer
+    191304, -- Sturdy Expedition Shovel
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
     194483, -- Plans: Obsidian Seared Slicer
+    199239, -- Schematic: Tinker: Alarm-O-Turret
     201734, -- Technique: Cliffside Wylderdrake: Silver and Blue Armor
     222573, -- Lightweight Scribe's Quill
     223051, -- Plans: Artisan Skinning Knife
@@ -16,7 +19,6 @@ local ItemIDs = {
     223061, -- Technique: Inscribed Rolling Pin
     223085, -- Design: Fractured Gemstone Locket
     223086, -- Design: Insightful Blasphemite
-    223087, -- Design: Culminating Blasphemite
     223096, -- Pattern: Roiling Thunderstrike Talons
     223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
