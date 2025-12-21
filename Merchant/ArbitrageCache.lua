dac9b2681e1690a0a6f521d96c737181deba0a3c
Erik Bryant <erikbryantology@gmail.com>
2025-12-20 21:13:40 -0800

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,29 +1,12 @@
 local ItemIDs = {
-    4499, -- Huge Brown Sack
-    21283, -- Grimoire of Corruption
-    21939, -- Fel Elemental Rod
-    29363, -- Mana Wyrmling
-    86281, -- Pattern: Nightfire Robe
-    87408, -- Plans: Unyielding Bloodplate
+    44822, -- Albino Snake
     94573, -- Direhorn Runt
-    191237, -- Draconium Blacksmith's Hammer
-    191304, -- Sturdy Expedition Shovel
-    191578, -- Recipe: Transmute: Awakened Fire
-    191580, -- Recipe: Transmute: Awakened Earth
-    194483, -- Plans: Obsidian Seared Slicer
-    221797, -- Bismuth-Fueled Samophlange
-    222573, -- Lightweight Scribe's Quill
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
-    223061, -- Technique: Inscribed Rolling Pin
     223085, -- Design: Fractured Gemstone Locket
-    223086, -- Design: Insightful Blasphemite
+    223087, -- Design: Culminating Blasphemite
     223096, -- Pattern: Roiling Thunderstrike Talons
-    223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
-    223102, -- Pattern: Busy Bee's Buckle
-    224424, -- Pattern: Artisan Chef's Hat
-    224434, -- Pattern: Dawnthread Lining
 }
 
 ArbitrageCache = {
