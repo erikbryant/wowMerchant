5fe0cc57d23768a6ca39a95de48d97992ea3b9d9
Erik Bryant <erikbryantology@gmail.com>
2025-12-27 17:48:13 -0800

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,12 +1,29 @@
 local ItemIDs = {
+    12833, -- Plans: Hammer of the Titans
+    16248, -- Formula: Enchant Weapon - Unholy
+    21841, -- Netherweave Bag
+    41788, -- Design: Beaming Earthsiege Diamond
     44822, -- Albino Snake
+    86238, -- Pattern: Chestguard of Nemeses
+    87408, -- Plans: Unyielding Bloodplate
     94573, -- Direhorn Runt
+    118427, -- Autographed Hearthstone Card
+    191236, -- Draconium Leatherworker's Toolset
+    191238, -- Draconium Leatherworker's Knife
+    191578, -- Recipe: Transmute: Awakened Fire
+    191580, -- Recipe: Transmute: Awakened Earth
+    221797, -- Bismuth-Fueled Samophlange
+    222573, -- Lightweight Scribe's Quill
+    222575, -- Hasty Alchemist's Mixing Rod
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
     223085, -- Design: Fractured Gemstone Locket
+    223086, -- Design: Insightful Blasphemite
     223087, -- Design: Culminating Blasphemite
     223096, -- Pattern: Roiling Thunderstrike Talons
+    223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
+    224020, -- Recipe: Vicious Flask of the Wrecking Ball
 }
 
 ArbitrageCache = {
