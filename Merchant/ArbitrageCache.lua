1d9b7225124e151ca2cf7ad691dd6ada03bbc256
Erik Bryant <erikbryantology@gmail.com>
2026-01-03 13:49:20 -0800

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,19 +1,19 @@
 local ItemIDs = {
     10050, -- Mageweave Bag
     21841, -- Netherweave Bag
-    30324, -- Plans: Red Havoc Boots
     41788, -- Design: Beaming Earthsiege Diamond
     44822, -- Albino Snake
     86238, -- Pattern: Chestguard of Nemeses
+    86272, -- Pattern: Fists of Lightning
     86284, -- Pattern: Raven Lord's Gloves
     87408, -- Plans: Unyielding Bloodplate
     94573, -- Direhorn Runt
     118427, -- Autographed Hearthstone Card
     191236, -- Draconium Leatherworker's Toolset
-    191242, -- Draconium Pickaxe
+    191240, -- Draconium Skinning Knife
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
-    201410, -- Powerful Purple Thing
+    194476, -- Plans: Obsidian Seared Hexsword
     221797, -- Bismuth-Fueled Samophlange
     222573, -- Lightweight Scribe's Quill
     223051, -- Plans: Artisan Skinning Knife
@@ -21,11 +21,11 @@ local ItemIDs = {
     223085, -- Design: Fractured Gemstone Locket
     223086, -- Design: Insightful Blasphemite
     223087, -- Design: Culminating Blasphemite
-    223096, -- Pattern: Roiling Thunderstrike Talons
     223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
     223102, -- Pattern: Busy Bee's Buckle
     224434, -- Pattern: Dawnthread Lining
+    225476, -- Technique: Algari Competitor's Insignia of Alacrity
 }
 
 ArbitrageCache = {
