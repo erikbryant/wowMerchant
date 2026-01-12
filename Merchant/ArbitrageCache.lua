32a684fe368c759a7c5aea1b201520d2222f58f7
Erik Bryant <erikbryantology@gmail.com>
2026-01-11 18:57:39 -0800

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,13 +1,12 @@
 local ItemIDs = {
     10050, -- Mageweave Bag
     23817, -- Schematic: Titanium Toolbox
-    41788, -- Design: Beaming Earthsiege Diamond
     44822, -- Albino Snake
-    86238, -- Pattern: Chestguard of Nemeses
     86284, -- Pattern: Raven Lord's Gloves
     87408, -- Plans: Unyielding Bloodplate
     94573, -- Direhorn Runt
     118427, -- Autographed Hearthstone Card
+    136972, -- Tome of Hex: Cockroach
     191236, -- Draconium Leatherworker's Toolset
     191237, -- Draconium Blacksmith's Hammer
     191240, -- Draconium Skinning Knife
@@ -17,13 +16,14 @@ local ItemIDs = {
     221797, -- Bismuth-Fueled Samophlange
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
+    223061, -- Technique: Inscribed Rolling Pin
     223085, -- Design: Fractured Gemstone Locket
-    223086, -- Design: Insightful Blasphemite
     223087, -- Design: Culminating Blasphemite
     223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
     223102, -- Pattern: Busy Bee's Buckle
     224434, -- Pattern: Dawnthread Lining
+    225473, -- Schematic: Algari Competitor's Mail Bracers
     225476, -- Technique: Algari Competitor's Insignia of Alacrity
 }
 
