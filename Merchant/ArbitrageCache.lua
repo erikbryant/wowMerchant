05ba91e303dcfe656c5a758ed325b443c4134948
Erik Bryant <erikbryantology@gmail.com>
2026-01-19 16:15:12 -0800

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,28 +1,30 @@
 local ItemIDs = {
-    10050, -- Mageweave Bag
     23817, -- Schematic: Titanium Toolbox
-    44822, -- Albino Snake
+    30324, -- Plans: Red Havoc Boots
+    60335, -- Thick Hide Pack
+    86283, -- Pattern: Raiment of Blood and Bone
     86284, -- Pattern: Raven Lord's Gloves
-    87408, -- Plans: Unyielding Bloodplate
+    87410, -- Plans: Ornate Battleplate of the Master
     94573, -- Direhorn Runt
     118427, -- Autographed Hearthstone Card
     136972, -- Tome of Hex: Cockroach
-    191236, -- Draconium Leatherworker's Toolset
     191237, -- Draconium Blacksmith's Hammer
     191240, -- Draconium Skinning Knife
+    191241, -- Draconium Sickle
+    191304, -- Sturdy Expedition Shovel
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
     199239, -- Schematic: Tinker: Alarm-O-Turret
-    221797, -- Bismuth-Fueled Samophlange
+    208302, -- Pattern: Verdant Combatant's Adamant Epaulets
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
     223061, -- Technique: Inscribed Rolling Pin
     223085, -- Design: Fractured Gemstone Locket
     223087, -- Design: Culminating Blasphemite
+    223096, -- Pattern: Roiling Thunderstrike Talons
     223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
     223102, -- Pattern: Busy Bee's Buckle
-    224434, -- Pattern: Dawnthread Lining
     225473, -- Schematic: Algari Competitor's Mail Bracers
     225476, -- Technique: Algari Competitor's Insignia of Alacrity
 }
