72295a502d14852f062c9d2e75bd83c320d03cbe
Erik Bryant <erikbryantology@gmail.com>
2026-01-24 09:15:46 -0800

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,18 +1,19 @@
 local ItemIDs = {
     23817, -- Schematic: Titanium Toolbox
     60335, -- Thick Hide Pack
+    87408, -- Plans: Unyielding Bloodplate
     87409, -- Plans: Gauntlets of Battle Command
     87410, -- Plans: Ornate Battleplate of the Master
     94573, -- Direhorn Runt
     118427, -- Autographed Hearthstone Card
     136972, -- Tome of Hex: Cockroach
-    158879, -- Bloodstained Message
     191237, -- Draconium Blacksmith's Hammer
     191240, -- Draconium Skinning Knife
     191241, -- Draconium Sickle
     191304, -- Sturdy Expedition Shovel
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
+    198715, -- Runed Draconium Rod
     199239, -- Schematic: Tinker: Alarm-O-Turret
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
@@ -24,7 +25,6 @@ local ItemIDs = {
     223101, -- Pattern: Reinforced Setae Flyers
     223102, -- Pattern: Busy Bee's Buckle
     225473, -- Schematic: Algari Competitor's Mail Bracers
-    225476, -- Technique: Algari Competitor's Insignia of Alacrity
 }
 
 ArbitrageCache = {
