c09bc7ee3427d8db2436fc9d4a990fb5e12dafb1
Erik Bryant <erikbryantology@gmail.com>
2026-01-28 08:37:31 -0800

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,13 +1,12 @@
 local ItemIDs = {
     12833, -- Plans: Hammer of the Titans
-    23817, -- Schematic: Titanium Toolbox
     30306, -- Pattern: Boots of Utter Darkness
-    30308, -- Pattern: Hurricane Boots
-    48120, -- Obsidian Hatchling
+    30323, -- Plans: Boots of the Protector
     60335, -- Thick Hide Pack
     86280, -- Pattern: Murderer's Gloves
     87410, -- Plans: Ornate Battleplate of the Master
     87411, -- Plans: Bloodforged Warfists
+    94573, -- Direhorn Runt
     118427, -- Autographed Hearthstone Card
     119212, -- Winning Hand
     191237, -- Draconium Blacksmith's Hammer
@@ -20,7 +19,6 @@ local ItemIDs = {
     223060, -- Technique: Patient Alchemist's Mixing Rod
     223061, -- Technique: Inscribed Rolling Pin
     223085, -- Design: Fractured Gemstone Locket
-    223087, -- Design: Culminating Blasphemite
     223096, -- Pattern: Roiling Thunderstrike Talons
     223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
