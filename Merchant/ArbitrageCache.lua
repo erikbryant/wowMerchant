bde93744fa0f5f1cc32d2b0d296e8325fd478be1
Erik Bryant <erikbryantology@gmail.com>
2026-02-01 19:02:55 -0800

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -3,29 +3,28 @@ local ItemIDs = {
     30306, -- Pattern: Boots of Utter Darkness
     30323, -- Plans: Boots of the Protector
     44822, -- Albino Snake
-    60335, -- Thick Hide Pack
+    48120, -- Obsidian Hatchling
     86280, -- Pattern: Murderer's Gloves
-    87409, -- Plans: Gauntlets of Battle Command
     87410, -- Plans: Ornate Battleplate of the Master
     87411, -- Plans: Bloodforged Warfists
     94573, -- Direhorn Runt
-    118427, -- Autographed Hearthstone Card
+    117445, -- Clefthoof Hide Satchel
     119212, -- Winning Hand
-    191237, -- Draconium Blacksmith's Hammer
     191304, -- Sturdy Expedition Shovel
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
-    194276, -- Pattern: Crimson Combatant's Wildercloth Shoulderpads
     198715, -- Runed Draconium Rod
     215121, -- Incanter's Shard
     221797, -- Bismuth-Fueled Samophlange
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
     223085, -- Design: Fractured Gemstone Locket
+    223087, -- Design: Culminating Blasphemite
     223096, -- Pattern: Roiling Thunderstrike Talons
     223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
     223102, -- Pattern: Busy Bee's Buckle
+    224434, -- Pattern: Dawnthread Lining
 }
 
 ArbitrageCache = {
