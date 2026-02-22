606bf99a8a9649315c2017c2bce1bb32e473c0f1
Erik Bryant <erikbryantology@gmail.com>
2026-02-21 18:07:16 -0800

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,14 +1,14 @@
 local ItemIDs = {
-    30306, -- Pattern: Boots of Utter Darkness
     67387, -- "Carriage" Signature Bag
     86279, -- Pattern: Liferuned Leather Gloves
     86280, -- Pattern: Murderer's Gloves
     86284, -- Pattern: Raven Lord's Gloves
-    87411, -- Plans: Bloodforged Warfists
+    86382, -- Pattern: Touch of the Light
     94573, -- Direhorn Runt
     117445, -- Clefthoof Hide Satchel
     118427, -- Autographed Hearthstone Card
     191237, -- Draconium Blacksmith's Hammer
+    191238, -- Draconium Leatherworker's Knife
     191304, -- Sturdy Expedition Shovel
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
