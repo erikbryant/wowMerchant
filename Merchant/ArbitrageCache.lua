0ba0d1f51cc4f31db2f53d2f104476f6e2bf3531
Erik Bryant <erikbryantology@gmail.com>
2026-02-24 11:59:33 -0800

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,19 +1,25 @@
 local ItemIDs = {
+    21288, -- Libram: Blessing of Wisdom
+    21290, -- Libram: Holy Light
+    21298, -- Manual of Battle Shout
+    21307, -- Guide: Aspect of the Hawk
+    30306, -- Pattern: Boots of Utter Darkness
+    30307, -- Pattern: Boots of the Crimson Hawk
     67387, -- "Carriage" Signature Bag
+    86238, -- Pattern: Chestguard of Nemeses
     86279, -- Pattern: Liferuned Leather Gloves
-    86280, -- Pattern: Murderer's Gloves
     86284, -- Pattern: Raven Lord's Gloves
     86382, -- Pattern: Touch of the Light
     94573, -- Direhorn Runt
     117445, -- Clefthoof Hide Satchel
     118427, -- Autographed Hearthstone Card
+    146953, -- Scraps
     191237, -- Draconium Blacksmith's Hammer
     191238, -- Draconium Leatherworker's Knife
     191304, -- Sturdy Expedition Shovel
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
-    215121, -- Incanter's Shard
-    221797, -- Bismuth-Fueled Samophlange
+    211589, -- Design: Draconic Combatant's Jeweled Signet
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
     223085, -- Design: Fractured Gemstone Locket
