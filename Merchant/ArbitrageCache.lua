169f36cef160f0a6e2931e9f0aa04de140d9d30f
Erik Bryant <erikbryantology@gmail.com>
2026-02-28 09:57:10 -0800

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -2,7 +2,6 @@ local ItemIDs = {
     21841, -- Netherweave Bag
     30305, -- Pattern: Boots of Natural Grace
     30307, -- Pattern: Boots of the Crimson Hawk
-    86238, -- Pattern: Chestguard of Nemeses
     86382, -- Pattern: Touch of the Light
     87411, -- Plans: Bloodforged Warfists
     87412, -- Plans: Chestplate of Limitless Faith
@@ -13,7 +12,6 @@ local ItemIDs = {
     191240, -- Draconium Skinning Knife
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
-    199228, -- Schematic: Grease Grenade
     221797, -- Bismuth-Fueled Samophlange
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
@@ -24,7 +22,6 @@ local ItemIDs = {
     223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
     223102, -- Pattern: Busy Bee's Buckle
-    224114, -- Runed Bismuth Rod
     224434, -- Pattern: Dawnthread Lining
     263519, -- Snake Oil
 }
