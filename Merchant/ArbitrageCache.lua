b8e553cf2c2819e2ff0b34f8f5f756d97186971f
Erik Bryant <erikbryantology@gmail.com>
2026-03-03 07:21:44 -0800

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -7,12 +7,13 @@ local ItemIDs = {
     87412, -- Plans: Chestplate of Limitless Faith
     87413, -- Plans: Gauntlets of Unbound Devotion
     94573, -- Direhorn Runt
-    191235, -- Draconium Blacksmith's Toolbox
     191236, -- Draconium Leatherworker's Toolset
     191237, -- Draconium Blacksmith's Hammer
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
+    194484, -- Plans: Infurious Helm of Vengeance
     221797, -- Bismuth-Fueled Samophlange
+    222577, -- Burnt Rolling Pin
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
     223085, -- Design: Fractured Gemstone Locket
@@ -24,6 +25,7 @@ local ItemIDs = {
     223102, -- Pattern: Busy Bee's Buckle
     224018, -- Recipe: Vicious Flask of Classical Spirits
     224434, -- Pattern: Dawnthread Lining
+    254749, -- Phial of Burrow Balm
     263515, -- Broken Soulstealing Mirror
 }
 
