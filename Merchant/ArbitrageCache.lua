bce1cd19697c42dacbbcdf32622c05b44ada7efd
Erik Bryant <erikbryantology@gmail.com>
2026-03-03 09:59:04 -0800

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -11,14 +11,12 @@ local ItemIDs = {
     191237, -- Draconium Blacksmith's Hammer
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
-    194484, -- Plans: Infurious Helm of Vengeance
     221797, -- Bismuth-Fueled Samophlange
     222577, -- Burnt Rolling Pin
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
     223085, -- Design: Fractured Gemstone Locket
     223086, -- Design: Insightful Blasphemite
-    223087, -- Design: Culminating Blasphemite
     223096, -- Pattern: Roiling Thunderstrike Talons
     223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
