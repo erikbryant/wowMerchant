0a3be2323e899073cd47f1acf14043b2c925c708
Erik Bryant <erikbryantology@gmail.com>
2026-03-08 19:46:23 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,13 +1,15 @@
 local ItemIDs = {
     21841, -- Netherweave Bag
-    30305, -- Pattern: Boots of Natural Grace
     30307, -- Pattern: Boots of the Crimson Hawk
     30323, -- Plans: Boots of the Protector
+    86281, -- Pattern: Nightfire Robe
     87412, -- Plans: Chestplate of Limitless Faith
     87413, -- Plans: Gauntlets of Unbound Devotion
     94573, -- Direhorn Runt
+    191235, -- Draconium Blacksmith's Toolbox
     191236, -- Draconium Leatherworker's Toolset
     191237, -- Draconium Blacksmith's Hammer
+    191304, -- Sturdy Expedition Shovel
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
     221797, -- Bismuth-Fueled Samophlange
@@ -21,9 +23,9 @@ local ItemIDs = {
     223101, -- Pattern: Reinforced Setae Flyers
     223102, -- Pattern: Busy Bee's Buckle
     224018, -- Recipe: Vicious Flask of Classical Spirits
-    224434, -- Pattern: Dawnthread Lining
     254749, -- Phial of Burrow Balm
     263515, -- Broken Soulstealing Mirror
+    267395, -- Technique: Homely Wall Shelves
 }
 
 ArbitrageCache = {
