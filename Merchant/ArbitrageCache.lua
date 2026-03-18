058307c799d47a7be7edda7bfb8b73a383bf9994
Erik Bryant <erikbryantology@gmail.com>
2026-03-18 08:02:33 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,13 +1,14 @@
 local ItemIDs = {
     30323, -- Plans: Boots of the Protector
-    87411, -- Plans: Bloodforged Warfists
-    87413, -- Plans: Gauntlets of Unbound Devotion
     94573, -- Direhorn Runt
     118427, -- Autographed Hearthstone Card
     191236, -- Draconium Leatherworker's Toolset
     191237, -- Draconium Blacksmith's Hammer
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
+    193880, -- Pattern: Wind Spirit's Lasso
+    194642, -- Design: Choker of Shielding
+    198910, -- Technique: Illusion Parchment: Shadow Orb
     221797, -- Bismuth-Fueled Samophlange
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
@@ -19,6 +20,7 @@ local ItemIDs = {
     223101, -- Pattern: Reinforced Setae Flyers
     223102, -- Pattern: Busy Bee's Buckle
     240953, -- Bold Biographer's Bifocals
+    240954, -- Fantastic Font Focuser
     254749, -- Phial of Burrow Balm
     256761, -- Formula: Enchant Shoulders - Voidtouched
     258134, -- Pattern: Voidstrider Saddlebag
