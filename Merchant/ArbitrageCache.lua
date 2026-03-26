c0883740781b0ed4b95e788bfb5e224f61059625
Erik Bryant <erikbryantology@gmail.com>
2026-03-26 08:35:33 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,15 +1,18 @@
 local ItemIDs = {
+    30305, -- Pattern: Boots of Natural Grace
     44822, -- Albino Snake
     86283, -- Pattern: Raiment of Blood and Bone
     94573, -- Direhorn Runt
     118427, -- Autographed Hearthstone Card
     191237, -- Draconium Blacksmith's Hammer
     191239, -- Draconium Needle Set
-    191304, -- Sturdy Expedition Shovel
+    191242, -- Draconium Pickaxe
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
+    193528, -- Wildercloth Alchemist's Robe
     194642, -- Design: Choker of Shielding
     198910, -- Technique: Illusion Parchment: Shadow Orb
+    201967, -- Autographed Skull
     221797, -- Bismuth-Fueled Samophlange
     223060, -- Technique: Patient Alchemist's Mixing Rod
     223085, -- Design: Fractured Gemstone Locket
@@ -18,6 +21,7 @@ local ItemIDs = {
     223096, -- Pattern: Roiling Thunderstrike Talons
     223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
+    223102, -- Pattern: Busy Bee's Buckle
     240953, -- Bold Biographer's Bifocals
     240954, -- Fantastic Font Focuser
     254749, -- Phial of Burrow Balm
