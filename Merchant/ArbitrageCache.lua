af542ed171d645daf95baa2f53b1d1cd194309dd
Erik Bryant <erikbryantology@gmail.com>
2026-03-30 08:37:02 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,15 +1,14 @@
 local ItemIDs = {
-    30305, -- Pattern: Boots of Natural Grace
     44822, -- Albino Snake
     86283, -- Pattern: Raiment of Blood and Bone
+    86382, -- Pattern: Touch of the Light
+    87409, -- Plans: Gauntlets of Battle Command
+    87413, -- Plans: Gauntlets of Unbound Devotion
     94573, -- Direhorn Runt
-    118427, -- Autographed Hearthstone Card
     191237, -- Draconium Blacksmith's Hammer
     191239, -- Draconium Needle Set
-    191242, -- Draconium Pickaxe
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
-    193528, -- Wildercloth Alchemist's Robe
     194642, -- Design: Choker of Shielding
     198910, -- Technique: Illusion Parchment: Shadow Orb
     201967, -- Autographed Skull
@@ -17,17 +16,16 @@ local ItemIDs = {
     223060, -- Technique: Patient Alchemist's Mixing Rod
     223085, -- Design: Fractured Gemstone Locket
     223086, -- Design: Insightful Blasphemite
-    223087, -- Design: Culminating Blasphemite
     223096, -- Pattern: Roiling Thunderstrike Talons
     223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
-    223102, -- Pattern: Busy Bee's Buckle
+    224017, -- Recipe: Vicious Flask of Honor
     240953, -- Bold Biographer's Bifocals
     240954, -- Fantastic Font Focuser
-    254749, -- Phial of Burrow Balm
     256761, -- Formula: Enchant Shoulders - Voidtouched
     258134, -- Pattern: Voidstrider Saddlebag
     258499, -- Technique: Restful Bronze Bench
+    263519, -- Snake Oil
     267395, -- Technique: Homely Wall Shelves
 }
 
