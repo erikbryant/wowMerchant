65c6107f15abbb7f1b3469725fe9adf7cecf89f0
Erik Bryant <erikbryantology@gmail.com>
2026-04-02 08:34:02 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,14 +1,16 @@
 local ItemIDs = {
+    30323, -- Plans: Boots of the Protector
     44822, -- Albino Snake
     86283, -- Pattern: Raiment of Blood and Bone
     86382, -- Pattern: Touch of the Light
-    87409, -- Plans: Gauntlets of Battle Command
-    87413, -- Plans: Gauntlets of Unbound Devotion
+    87412, -- Plans: Chestplate of Limitless Faith
     94573, -- Direhorn Runt
-    191237, -- Draconium Blacksmith's Hammer
+    117445, -- Clefthoof Hide Satchel
     191239, -- Draconium Needle Set
+    191304, -- Sturdy Expedition Shovel
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
+    193880, -- Pattern: Wind Spirit's Lasso
     194642, -- Design: Choker of Shielding
     198910, -- Technique: Illusion Parchment: Shadow Orb
     201967, -- Autographed Skull
@@ -17,9 +19,7 @@ local ItemIDs = {
     223085, -- Design: Fractured Gemstone Locket
     223086, -- Design: Insightful Blasphemite
     223096, -- Pattern: Roiling Thunderstrike Talons
-    223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
-    224017, -- Recipe: Vicious Flask of Honor
     240953, -- Bold Biographer's Bifocals
     240954, -- Fantastic Font Focuser
     256761, -- Formula: Enchant Shoulders - Voidtouched
