ddc7b493f85a114e3adc6e24b020be64d70433bb
Erik Bryant <erikbryantology@gmail.com>
2026-04-04 10:56:45 -0700

    Mute some in-game dialog

--- Merchant/Merchant.lua
+++ Merchant/Merchant.lua
@@ -1,1 +1,6 @@
+-- Hakmud of Argus, Traveling Trader
+MuteSoundFile(557322) -- Remember the lessons of the past
+MuteSoundFile(557338) -- <Foreign words>
+MuteSoundFile(557334) -- Be kind to those less fortunate
+
 MerchUtil.PrettyPrint("Loaded and ready to sell!  ["..MerchGlobal.SLASH_CMD.."]")
