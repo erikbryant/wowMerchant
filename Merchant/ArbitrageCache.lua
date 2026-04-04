3acb901a2ad5293b43e2e71598a4fdf525cb1784
Erik Bryant <erikbryantology@gmail.com>
2026-04-04 13:13:56 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,31 +1,38 @@
 local ItemIDs = {
+    4238, -- Linen Bag
+    4240, -- Woolen Bag
+    5571, -- Small Black Pouch
+    12713, -- Plans: Radiant Leggings
+    16248, -- Formula: Enchant Weapon - Unholy
+    30307, -- Pattern: Boots of the Crimson Hawk
     30323, -- Plans: Boots of the Protector
-    44822, -- Albino Snake
+    72001, -- Plans: Pyrium Legplates of Purified Evil
+    86279, -- Pattern: Liferuned Leather Gloves
     86283, -- Pattern: Raiment of Blood and Bone
     86382, -- Pattern: Touch of the Light
-    87412, -- Plans: Chestplate of Limitless Faith
+    87408, -- Plans: Unyielding Bloodplate
     94573, -- Direhorn Runt
     117445, -- Clefthoof Hide Satchel
+    191238, -- Draconium Leatherworker's Knife
     191239, -- Draconium Needle Set
-    191304, -- Sturdy Expedition Shovel
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
-    193880, -- Pattern: Wind Spirit's Lasso
-    194642, -- Design: Choker of Shielding
     198910, -- Technique: Illusion Parchment: Shadow Orb
-    201967, -- Autographed Skull
-    221797, -- Bismuth-Fueled Samophlange
+    223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
+    223061, -- Technique: Inscribed Rolling Pin
     223085, -- Design: Fractured Gemstone Locket
     223086, -- Design: Insightful Blasphemite
     223096, -- Pattern: Roiling Thunderstrike Talons
+    223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
     240953, -- Bold Biographer's Bifocals
     240954, -- Fantastic Font Focuser
+    244618, -- Tinker's Handguard
+    244717, -- Junker's Multitool
     256761, -- Formula: Enchant Shoulders - Voidtouched
     258134, -- Pattern: Voidstrider Saddlebag
     258499, -- Technique: Restful Bronze Bench
-    263519, -- Snake Oil
     267395, -- Technique: Homely Wall Shelves
 }
 
