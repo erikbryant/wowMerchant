6edf5e6f2d79eeb64c0166ee538f79779e8e6a03
Erik Bryant <erikbryantology@gmail.com>
2026-04-11 14:13:40 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,35 +1,29 @@
 local ItemIDs = {
     4238, -- Linen Bag
     4240, -- Woolen Bag
-    5571, -- Small Black Pouch
-    12713, -- Plans: Radiant Leggings
-    16248, -- Formula: Enchant Weapon - Unholy
-    30307, -- Pattern: Boots of the Crimson Hawk
-    30323, -- Plans: Boots of the Protector
-    72001, -- Plans: Pyrium Legplates of Purified Evil
-    86279, -- Pattern: Liferuned Leather Gloves
-    86283, -- Pattern: Raiment of Blood and Bone
-    86382, -- Pattern: Touch of the Light
-    87408, -- Plans: Unyielding Bloodplate
-    94573, -- Direhorn Runt
-    117445, -- Clefthoof Hide Satchel
-    191238, -- Draconium Leatherworker's Knife
-    191239, -- Draconium Needle Set
-    191578, -- Recipe: Transmute: Awakened Fire
+    4292, -- Pattern: Green Woolen Bag
+    4499, -- Huge Brown Sack
+    9294, -- Recipe: Wildvine Potion
+    17706, -- Plans: Edge of Winter
+    22307, -- Pattern: Enchanted Mageweave Pouch
+    29532, -- Drums of Panic
+    30308, -- Pattern: Hurricane Boots
+    30324, -- Plans: Red Havoc Boots
+    44446, -- Pack of Endless Pockets
+    86272, -- Pattern: Fists of Lightning
+    86280, -- Pattern: Murderer's Gloves
+    112384, -- Reflecting Prism
     191580, -- Recipe: Transmute: Awakened Earth
-    198910, -- Technique: Illusion Parchment: Shadow Orb
     223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
-    223061, -- Technique: Inscribed Rolling Pin
     223085, -- Design: Fractured Gemstone Locket
-    223086, -- Design: Insightful Blasphemite
-    223096, -- Pattern: Roiling Thunderstrike Talons
-    223098, -- Pattern: Waders of the Unifying Flame
     223101, -- Pattern: Reinforced Setae Flyers
+    237952, -- Sun-Blessed Blacksmith's Toolbox
+    238012, -- Thalassian Leatherworker's Knife
     240953, -- Bold Biographer's Bifocals
     240954, -- Fantastic Font Focuser
-    244618, -- Tinker's Handguard
-    244717, -- Junker's Multitool
+    256625, -- Pattern: Hexwoven Strand
+    256637, -- Pattern: Axe-Flingin' Bands
     256761, -- Formula: Enchant Shoulders - Voidtouched
     258134, -- Pattern: Voidstrider Saddlebag
     258499, -- Technique: Restful Bronze Bench
