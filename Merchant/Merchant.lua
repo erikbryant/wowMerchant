c710244600ca674aa661584741b499a7ccce4944
Erik Bryant <erikbryantology@gmail.com>
2026-04-12 10:36:57 -0700

    Disable annoying sounds

--- Merchant/Merchant.lua
+++ Merchant/Merchant.lua
@@ -1,6 +1,39 @@
--- Hakmud of Argus, Traveling Trader
+hooksecurefunc("PlaySound", function(id) print("SoundKit ID played: "..id) end)
+hooksecurefunc("PlaySoundFile", function(id) print("FileDataID played: "..id) end)
+
+-- Hakmud of Argus, Traveling Trader (_Farewell)
 MuteSoundFile(557322) -- Remember the lessons of the past
 MuteSoundFile(557338) -- <Foreign words>
 MuteSoundFile(557334) -- Be kind to those less fortunate
+
+-- Female Worgen dialog (VO_WorgenCivF_Greeting)
+MuteSoundFile(564320) -- Yes?
+MuteSoundFile(564309) -- What can I do for you?
+MuteSoundFile(564298) -- I've got a bad feeling
+MuteSoundFile(564303) -- Get gabbing or get going
+MuteSoundFile(564328) -- What's your story?
+MuteSoundFile(564299) -- Any friend of Greymane is a friend of mine
+MuteSoundFile(564308) -- Ain't you a chipper looking one?
+MuteSoundFile(564297) -- We've been walled up for far too long
+
+-- Female Worgen dialog (VO_WorgenCivF_Farewell)
+MuteSoundFile(564301) -- That's enough gabbing for me today
+MuteSoundFile(564307) -- Keep your chin up, eh?
+MuteSoundFile(564324) -- You would not want to see me when I am angry
+MuteSoundFile(564302) -- Long live Greymane!
+MuteSoundFile(564313) -- Farewell
+MuteSoundFile(564300) -- Watch your back
+MuteSoundFile(564291) -- We are bound by a common enemy
+MuteSoundFile(564330) -- Good day
+MuteSoundFile(564292) -- Let the light of the new moon guide you
+
+-- Clock tower hourly bell
+MuteSoundFile(6594) -- Alliance
+MuteSoundFile(133120) -- Alliance
+MuteSoundFile(6595) -- Horde
+MuteSoundFile(132646) -- Horde
+MuteSoundFile(9154) -- Generic / Karazhan
+MuteSoundFile(134107) -- Generic / Karazhan
+MuteSoundFile(6674) -- Night Elf
 
 MerchUtil.PrettyPrint("Loaded and ready to sell!  ["..MerchGlobal.SLASH_CMD.."]")
