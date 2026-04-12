ee3693e2bef6583b4034f9aa16907e9ab9ae90b6
Erik Bryant <erikbryantology@gmail.com>
2026-04-12 10:37:07 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,29 +1,26 @@
 local ItemIDs = {
-    4238, -- Linen Bag
-    4240, -- Woolen Bag
-    4292, -- Pattern: Green Woolen Bag
+    828, -- Small Blue Pouch
     4499, -- Huge Brown Sack
     9294, -- Recipe: Wildvine Potion
+    11610, -- Plans: Dark Iron Pulverizer
+    11612, -- Plans: Dark Iron Plate
+    16048, -- Schematic: Dark Iron Rifle
     17706, -- Plans: Edge of Winter
     22307, -- Pattern: Enchanted Mageweave Pouch
-    29532, -- Drums of Panic
+    30306, -- Pattern: Boots of Utter Darkness
     30308, -- Pattern: Hurricane Boots
     30324, -- Plans: Red Havoc Boots
-    44446, -- Pack of Endless Pockets
     86272, -- Pattern: Fists of Lightning
-    86280, -- Pattern: Murderer's Gloves
     112384, -- Reflecting Prism
+    191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
-    223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
     223085, -- Design: Fractured Gemstone Locket
     223101, -- Pattern: Reinforced Setae Flyers
     237952, -- Sun-Blessed Blacksmith's Toolbox
-    238012, -- Thalassian Leatherworker's Knife
     240953, -- Bold Biographer's Bifocals
     240954, -- Fantastic Font Focuser
     256625, -- Pattern: Hexwoven Strand
-    256637, -- Pattern: Axe-Flingin' Bands
     256761, -- Formula: Enchant Shoulders - Voidtouched
     258134, -- Pattern: Voidstrider Saddlebag
     258499, -- Technique: Restful Bronze Bench
