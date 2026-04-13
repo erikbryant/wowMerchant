f956957952dec76994158faa14babeabf5ba994d
Erik Bryant <erikbryantology@gmail.com>
2026-04-13 13:43:43 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,25 +1,22 @@
 local ItemIDs = {
-    828, -- Small Blue Pouch
-    4499, -- Huge Brown Sack
     9294, -- Recipe: Wildvine Potion
-    11610, -- Plans: Dark Iron Pulverizer
-    11612, -- Plans: Dark Iron Plate
     16048, -- Schematic: Dark Iron Rifle
-    17706, -- Plans: Edge of Winter
     22307, -- Pattern: Enchanted Mageweave Pouch
+    30283, -- Pattern: Boots of the Long Road
     30306, -- Pattern: Boots of Utter Darkness
-    30308, -- Pattern: Hurricane Boots
-    30324, -- Plans: Red Havoc Boots
     86272, -- Pattern: Fists of Lightning
+    86281, -- Pattern: Nightfire Robe
+    110289, -- Fat Sleeper Bait
+    110293, -- Abyssal Gulper Eel Bait
     112384, -- Reflecting Prism
+    191240, -- Draconium Skinning Knife
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
     223060, -- Technique: Patient Alchemist's Mixing Rod
     223085, -- Design: Fractured Gemstone Locket
     223101, -- Pattern: Reinforced Setae Flyers
-    237952, -- Sun-Blessed Blacksmith's Toolbox
-    240953, -- Bold Biographer's Bifocals
     240954, -- Fantastic Font Focuser
+    240955, -- Silvermoon Loupes
     256625, -- Pattern: Hexwoven Strand
     256761, -- Formula: Enchant Shoulders - Voidtouched
     258134, -- Pattern: Voidstrider Saddlebag
