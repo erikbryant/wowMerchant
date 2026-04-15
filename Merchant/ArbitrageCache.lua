c260324f9a97b97b38731b8f3f9b015e45f2124d
Erik Bryant <erikbryantology@gmail.com>
2026-04-14 22:13:44 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,15 +1,15 @@
 local ItemIDs = {
     9294, -- Recipe: Wildvine Potion
     16048, -- Schematic: Dark Iron Rifle
-    22307, -- Pattern: Enchanted Mageweave Pouch
+    23817, -- Schematic: Titanium Toolbox
     30283, -- Pattern: Boots of the Long Road
+    30305, -- Pattern: Boots of Natural Grace
     30306, -- Pattern: Boots of Utter Darkness
-    86272, -- Pattern: Fists of Lightning
+    41367, -- Dark Jade Focusing Lens
+    42420, -- Shadow Crystal Focusing Lens
     86281, -- Pattern: Nightfire Robe
-    94573, -- Direhorn Runt
     110289, -- Fat Sleeper Bait
     110293, -- Abyssal Gulper Eel Bait
-    112384, -- Reflecting Prism
     191240, -- Draconium Skinning Knife
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
@@ -18,6 +18,7 @@ local ItemIDs = {
     223101, -- Pattern: Reinforced Setae Flyers
     240953, -- Bold Biographer's Bifocals
     240954, -- Fantastic Font Focuser
+    244627, -- Apprentice Smith's Apron
     256761, -- Formula: Enchant Shoulders - Voidtouched
     258134, -- Pattern: Voidstrider Saddlebag
     258499, -- Technique: Restful Bronze Bench
