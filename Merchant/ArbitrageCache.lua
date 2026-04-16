53a2044dc7ccfe4c63d54a73b47e6da87bdcfe97
Erik Bryant <erikbryantology@gmail.com>
2026-04-15 22:22:33 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,24 +1,28 @@
 local ItemIDs = {
     9294, -- Recipe: Wildvine Potion
-    16048, -- Schematic: Dark Iron Rifle
+    12713, -- Plans: Radiant Leggings
+    20752, -- Formula: Minor Mana Oil
+    20753, -- Formula: Lesser Wizard Oil
+    20758, -- Formula: Minor Wizard Oil
+    22220, -- Plans: Black Grasp of the Destroyer
+    23811, -- Schematic: White Smoke Flare
     23817, -- Schematic: Titanium Toolbox
-    30283, -- Pattern: Boots of the Long Road
     30305, -- Pattern: Boots of Natural Grace
-    30306, -- Pattern: Boots of Utter Darkness
-    41367, -- Dark Jade Focusing Lens
-    42420, -- Shadow Crystal Focusing Lens
+    54446, -- Hyjal Expedition Bag
     86281, -- Pattern: Nightfire Robe
     110289, -- Fat Sleeper Bait
     110293, -- Abyssal Gulper Eel Bait
-    191240, -- Draconium Skinning Knife
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
+    193486, -- Resilient Smock
+    198910, -- Technique: Illusion Parchment: Shadow Orb
     223060, -- Technique: Patient Alchemist's Mixing Rod
+    223061, -- Technique: Inscribed Rolling Pin
     223085, -- Design: Fractured Gemstone Locket
     223101, -- Pattern: Reinforced Setae Flyers
     240953, -- Bold Biographer's Bifocals
     240954, -- Fantastic Font Focuser
-    244627, -- Apprentice Smith's Apron
+    256625, -- Pattern: Hexwoven Strand
     256761, -- Formula: Enchant Shoulders - Voidtouched
     258134, -- Pattern: Voidstrider Saddlebag
     258499, -- Technique: Restful Bronze Bench
