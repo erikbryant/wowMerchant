0e4eaae54fe2b120e1f9a5b6a46e3896c5345ad2
Erik Bryant <erikbryantology@gmail.com>
2026-04-16 13:34:23 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,11 +1,8 @@
 local ItemIDs = {
-    30305, -- Pattern: Boots of Natural Grace
-    198910, -- Technique: Illusion Parchment: Shadow Orb
-    223060, -- Technique: Patient Alchemist's Mixing Rod
+    191580, -- Recipe: Transmute: Awakened Earth
     223085, -- Design: Fractured Gemstone Locket
-    223096, -- Pattern: Roiling Thunderstrike Talons
-    240953, -- Bold Biographer's Bifocals
-    240954, -- Fantastic Font Focuser
+    238018, -- Sun-Blessed Blacksmith's Hammer
+    240959, -- Sin'dorei Jeweler's Loupes
     256761, -- Formula: Enchant Shoulders - Voidtouched
     258134, -- Pattern: Voidstrider Saddlebag
     258499, -- Technique: Restful Bronze Bench
