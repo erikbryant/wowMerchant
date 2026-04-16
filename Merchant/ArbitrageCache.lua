692ab4ff7cc5e8d3c03ce260ecdd4654c4a3c7c2
Erik Bryant <erikbryantology@gmail.com>
2026-04-16 09:21:21 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,20 +1,20 @@
 local ItemIDs = {
+    2657, -- Red Leather Bag
+    6663, -- Recipe: Elixir of Giant Growth
     9294, -- Recipe: Wildvine Potion
     12713, -- Plans: Radiant Leggings
+    17706, -- Plans: Edge of Winter
     20752, -- Formula: Minor Mana Oil
     20753, -- Formula: Lesser Wizard Oil
     20758, -- Formula: Minor Wizard Oil
     22220, -- Plans: Black Grasp of the Destroyer
-    23811, -- Schematic: White Smoke Flare
     23817, -- Schematic: Titanium Toolbox
     30305, -- Pattern: Boots of Natural Grace
     54446, -- Hyjal Expedition Bag
-    86281, -- Pattern: Nightfire Robe
     110289, -- Fat Sleeper Bait
-    110293, -- Abyssal Gulper Eel Bait
+    112384, -- Reflecting Prism
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
-    193486, -- Resilient Smock
     198910, -- Technique: Illusion Parchment: Shadow Orb
     223060, -- Technique: Patient Alchemist's Mixing Rod
     223061, -- Technique: Inscribed Rolling Pin
@@ -22,7 +22,6 @@ local ItemIDs = {
     223101, -- Pattern: Reinforced Setae Flyers
     240953, -- Bold Biographer's Bifocals
     240954, -- Fantastic Font Focuser
-    256625, -- Pattern: Hexwoven Strand
     256761, -- Formula: Enchant Shoulders - Voidtouched
     258134, -- Pattern: Voidstrider Saddlebag
     258499, -- Technique: Restful Bronze Bench
