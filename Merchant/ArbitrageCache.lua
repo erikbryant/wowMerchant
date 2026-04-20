5323b0dbeff456262d3c9ec9e122d6b1fabfcf5d
Erik Bryant <erikbryantology@gmail.com>
2026-04-20 13:46:34 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,14 +1,19 @@
 local ItemIDs = {
-    30324, -- Plans: Red Havoc Boots
     95408, -- Waterlogged Zandalari Journal
     95409, -- Iron-Bound Zandalari Journal
-    95411, -- Torn Zandalari Journal
     95412, -- Frayed Zandalari Journal
-    223060, -- Technique: Patient Alchemist's Mixing Rod
+    191237, -- Draconium Blacksmith's Hammer
+    191578, -- Recipe: Transmute: Awakened Fire
+    191580, -- Recipe: Transmute: Awakened Earth
+    223051, -- Plans: Artisan Skinning Knife
     223085, -- Design: Fractured Gemstone Locket
-    226643, -- Plans: Beledar's Bulwark
-    240954, -- Fantastic Font Focuser
-    258499, -- Technique: Restful Bronze Bench
+    223101, -- Pattern: Reinforced Setae Flyers
+    225484, -- Technique: Algari Competitor's Lamp
+    238012, -- Thalassian Leatherworker's Knife
+    244175, -- Runed Refulgent Copper Rod
+    244619, -- Hideworker's Cover
+    256761, -- Formula: Enchant Shoulders - Voidtouched
+    258134, -- Pattern: Voidstrider Saddlebag
     267395, -- Technique: Homely Wall Shelves
 }
 
