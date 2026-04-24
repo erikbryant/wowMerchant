2beb69162c15f87d26a05b8cad4728513e76a3b9
Erik Bryant <erikbryantology@gmail.com>
2026-04-24 05:05:04 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,14 +1,11 @@
 local ItemIDs = {
+    30308, -- Pattern: Hurricane Boots
     30323, -- Plans: Boots of the Protector
-    87409, -- Plans: Gauntlets of Battle Command
-    191242, -- Draconium Pickaxe
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
-    223060, -- Technique: Patient Alchemist's Mixing Rod
     223085, -- Design: Fractured Gemstone Locket
     223101, -- Pattern: Reinforced Setae Flyers
-    240953, -- Bold Biographer's Bifocals
-    256716, -- Design: Prismatic Focusing Iris
+    256653, -- Pattern: Ranger-General's Grips
     256761, -- Formula: Enchant Shoulders - Voidtouched
     258499, -- Technique: Restful Bronze Bench
     267395, -- Technique: Homely Wall Shelves
