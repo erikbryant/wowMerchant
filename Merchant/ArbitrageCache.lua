c9686ffa265fc7477b018c2de13e072a9d0fd72a
Erik Bryant <erikbryantology@gmail.com>
2026-04-25 07:29:41 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,12 +1,15 @@
 local ItemIDs = {
-    30308, -- Pattern: Hurricane Boots
-    30323, -- Plans: Boots of the Protector
+    191239, -- Draconium Needle Set
     191578, -- Recipe: Transmute: Awakened Fire
     191580, -- Recipe: Transmute: Awakened Earth
-    223085, -- Design: Fractured Gemstone Locket
+    199228, -- Schematic: Grease Grenade
+    223051, -- Plans: Artisan Skinning Knife
+    223060, -- Technique: Patient Alchemist's Mixing Rod
     223101, -- Pattern: Reinforced Setae Flyers
-    256653, -- Pattern: Ranger-General's Grips
-    256761, -- Formula: Enchant Shoulders - Voidtouched
+    238011, -- Thalassian Skinning Knife
+    239642, -- Chef's Bright Linen Cooking Chapeau
+    240953, -- Bold Biographer's Bifocals
+    258134, -- Pattern: Voidstrider Saddlebag
     258499, -- Technique: Restful Bronze Bench
     267395, -- Technique: Homely Wall Shelves
 }
