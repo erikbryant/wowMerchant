7ea1bb55609f8be0176186d0209501634e15a82f
Erik Bryant <erikbryantology@gmail.com>
2026-04-25 18:59:00 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,14 +1,13 @@
 local ItemIDs = {
-    191239, -- Draconium Needle Set
-    191578, -- Recipe: Transmute: Awakened Fire
+    30306, -- Pattern: Boots of Utter Darkness
+    165745, -- Crimson Ink Well
     191580, -- Recipe: Transmute: Awakened Earth
-    199228, -- Schematic: Grease Grenade
-    223051, -- Plans: Artisan Skinning Knife
     223060, -- Technique: Patient Alchemist's Mixing Rod
+    223085, -- Design: Fractured Gemstone Locket
     223101, -- Pattern: Reinforced Setae Flyers
-    238011, -- Thalassian Skinning Knife
-    239642, -- Chef's Bright Linen Cooking Chapeau
+    224020, -- Recipe: Vicious Flask of the Wrecking Ball
     240953, -- Bold Biographer's Bifocals
+    244709, -- Junker's Junk Visor
     258134, -- Pattern: Voidstrider Saddlebag
     258499, -- Technique: Restful Bronze Bench
     267395, -- Technique: Homely Wall Shelves
