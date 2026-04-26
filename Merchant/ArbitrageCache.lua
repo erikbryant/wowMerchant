d4e22dab79e044449074d60fcf7424d9593d7c3f
Erik Bryant <erikbryantology@gmail.com>
2026-04-26 08:02:17 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,14 +1,18 @@
 local ItemIDs = {
+    {14512, 1}, -- Pattern: Truefaith Vestments
     {191580, 1}, -- Recipe: Transmute: Awakened Earth
-    {219403, 1}, -- Stonebound Lantern
-    {223060, 1}, -- Technique: Patient Alchemist's Mixing Rod
     {223085, 1}, -- Design: Fractured Gemstone Locket
     {223101, 1}, -- Pattern: Reinforced Setae Flyers
     {224020, 1}, -- Recipe: Vicious Flask of the Wrecking Ball
-    {240953, 1}, -- Bold Biographer's Bifocals
-    {244709, 1}, -- Junker's Junk Visor
+    {240953, 180}, -- Bold Biographer's Bifocals
+    {240953, 186}, -- Bold Biographer's Bifocals
+    {240953, 192}, -- Bold Biographer's Bifocals
+    {240953, 199}, -- Bold Biographer's Bifocals
+    {240953, 206}, -- Bold Biographer's Bifocals
+    {256625, 1}, -- Pattern: Hexwoven Strand
     {258499, 1}, -- Technique: Restful Bronze Bench
     {267395, 1}, -- Technique: Homely Wall Shelves
+    {86382, 1}, -- Pattern: Touch of the Light
 }
 
 ArbitrageCache = {
