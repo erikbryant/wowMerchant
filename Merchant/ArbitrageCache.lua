3bbf4a6639646cb6b7000a07b8b8a47b8332bc4b
Erik Bryant <erikbryantology@gmail.com>
2026-04-27 09:45:54 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,18 +1,16 @@
 local ItemIDs = {
-    {14512, 1}, -- Pattern: Truefaith Vestments
+    {162588, 37}, -- Weathered Scrollcase
+    {191578, 1}, -- Recipe: Transmute: Awakened Fire
     {191580, 1}, -- Recipe: Transmute: Awakened Earth
     {223085, 1}, -- Design: Fractured Gemstone Locket
     {223101, 1}, -- Pattern: Reinforced Setae Flyers
-    {224020, 1}, -- Recipe: Vicious Flask of the Wrecking Ball
-    {240953, 180}, -- Bold Biographer's Bifocals
-    {240953, 186}, -- Bold Biographer's Bifocals
-    {240953, 192}, -- Bold Biographer's Bifocals
-    {240953, 199}, -- Bold Biographer's Bifocals
-    {240953, 206}, -- Bold Biographer's Bifocals
-    {256625, 1}, -- Pattern: Hexwoven Strand
-    {258499, 1}, -- Technique: Restful Bronze Bench
+    {223102, 1}, -- Pattern: Busy Bee's Buckle
+    {256653, 1}, -- Pattern: Ranger-General's Grips
+    {258134, 1}, -- Pattern: Voidstrider Saddlebag
     {267395, 1}, -- Technique: Homely Wall Shelves
-    {86382, 1}, -- Pattern: Touch of the Light
+    {48120, 1}, -- Obsidian Hatchling
+    {86283, 35}, -- Pattern: Raiment of Blood and Bone
+    {94573, 1}, -- Direhorn Runt
 }
 
 ArbitrageCache = {
