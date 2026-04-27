bbb43cb21dc377a5320c65d315e4965e9759ea35
Erik Bryant <erikbryantology@gmail.com>
2026-04-27 12:24:06 -0700

    Remove dead code. If item level is zero, let the AH default

--- Merchant/AHQuery.lua
+++ Merchant/AHQuery.lua
@@ -4,58 +4,6 @@ local ItemIDs = {}
 local ItemIDsIndex = 1
 local Scanning = false
 
-local IdToLevel = {
-    -- iLvl 80
-    {237948, {180, 186, 192, 199, 206}}, -- Thalassian Blacksmith's Toolbox (Profession)
-    {238011, {180, 186, 206}},           -- Thalassian Skinning Knife (Profession)
-    {238012, {180, 186, 192, 199, 206}}, -- Thalassian Leatherworker's Knife (Profession)
-    {239641, {180, 186, 192, 199, 206}}, -- Bright Linen Alchemy Apron (Profession)
-    {239642, {180, 186, 192, 206}},      -- Chef's Bright Linen Cooking Chapeau (Profession)
-    {239646, {180, 186, 192, 199, 206}}, -- Bright Linen Tailoring Robe (Profession)
-    {240953, {180, 186, 192, 199, 206}}, -- Bold Biographer's Bifocals (Profession)
-    {240954, {180, 186, 192, 199, 206}}, -- Fantastic Font Focuser (Profession)
-    {240955, {180, 183, 186, 189, 193}}, -- Silvermoon Loupes (Profession)
-    {244175, {180, 186, 192, 199, 206}}, -- Runed Refulgent Copper Rod (Profession)
-    {244618, {180, 186, 192, 199, 206}}, -- Tinker's Handguard (Profession)
-    {244619, {180, 186, 192, 199, 206}}, -- Hideworker's Cover (Profession)
-    {244627, {180, 186, 192, 199, 206}}, -- Apprentice Smith's Apron (Profession)
-
-    -- iLvl 106
-    {237952, {206, 212, 218, 225, 232}}, -- Sun-Blessed Blacksmith's Toolbox (Profession)
-    {238018, {212, 218, 225, 232}},      -- Sun-Blessed Blacksmith's Hammer (Profession)
-    {240959, {206, 212, 218, 225, 232}}, -- Sin'dorei Jeweler's Loupes (Profession)
-    {244718, {206, 212, 218, 225, 232}}, -- Turbo-Junker's Multitool v1 (Profession)
-
-    -- iLvl 317
-    {191235, {71, 72, 73, 74}},     -- Draconium Blacksmith's Toolbox (Profession)
-    {191236, {71, 72, 73, 74}},     -- Draconium Leatherworker's Toolset (Profession)
-    {191237, {70, 71, 72, 73, 74}}, -- Draconium Blacksmith's Hammer (Profession)
-    {191238, {71, 72, 73, 74}},     -- Draconium Leatherworker's Knife (Profession)
-    {191239, {71, 72, 73, 74}},     -- Draconium Needle Set (Profession)
-    {191240, {71, 72, 73, 74}},     -- Draconium Skinning Knife (Profession)
-    {191241, {70, 71, 72, 74}},     -- Draconium Sickle (Profession)
-    {191242, {70, 71, 72, 74}},     -- Draconium Pickaxe (Profession)
-    {193486, {70, 71, 72, 74}},     -- Resilient Smock (Profession)
-    {224114, {79, 85, 91, 105}},    -- Runed Bismuth Rod (Profession)
-
-    -- iLvl 486
-    {215120, {79, 85, 91, 98, 105}}, -- Radiant Loupes (Profession)
-    {221797, {79, 85, 91, 98, 105}}, -- Bismuth-Fueled Samophlange (Profession)
-    {244709, {180, 186, 192, 206}},  -- Junker's Junk Visor (Profession)
-
-    -- iLvl 535
-    {244626, {206, 212, 218, 225, 232}}, -- Sin'dorei Alchemist's Hat (Profession)
-}
-
-local function iLevels(itemID, default)
-    for i=1, #IdToLevel do
-        if itemID == IdToLevel[i][1] then
-            return IdToLevel[i][2]
-        end
-    end
-    return {default}
-end
-
 -- Display the favorites dialog
 local function ShowFavorites()
     local sorts = {
@@ -91,17 +39,16 @@ local function DefaultsOrFavorites()
     return itemIDs
 end
 
-local function LookupItemLevel(itemID, itemLevel)
-    return iLevels(itemID, itemLevel)[1]
-end
-
 -- Send a search query to the AH
 local function Send(itemID)
     local itemKey = C_AuctionHouse.MakeItemKey(itemID[1])
     local sorts = {
         {sortOrder=Enum.AuctionHouseSortOrder.Price, reverseSort=false},
     }
-    itemKey.itemLevel = itemID[2]
+    if itemID[2] ~= 0 then
+        -- We need a specific iLevel for this item
+        itemKey.itemLevel = itemID[2]
+    end
     C_AuctionHouse.SendSearchQuery(itemKey, sorts, false)
 end
 
