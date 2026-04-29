8ec08fd63d560c684b7c181cd20d80ea46bd3ff1
Erik Bryant <erikbryantology@gmail.com>
2026-04-28 17:01:55 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,16 +1,21 @@
 local ItemIDs = {
-    {162588, 37}, -- Weathered Scrollcase
-    {191578, 1}, -- Recipe: Transmute: Awakened Fire
-    {191580, 1}, -- Recipe: Transmute: Awakened Earth
-    {223085, 1}, -- Design: Fractured Gemstone Locket
-    {223101, 1}, -- Pattern: Reinforced Setae Flyers
-    {223102, 1}, -- Pattern: Busy Bee's Buckle
-    {256653, 1}, -- Pattern: Ranger-General's Grips
-    {258134, 1}, -- Pattern: Voidstrider Saddlebag
-    {267395, 1}, -- Technique: Homely Wall Shelves
-    {48120, 1}, -- Obsidian Hatchling
-    {86283, 35}, -- Pattern: Raiment of Blood and Bone
-    {94573, 1}, -- Direhorn Runt
+    {14513, 0}, -- Pattern: Robe of the Archmage
+    {221797, 105}, -- Bismuth-Fueled Samophlange
+    {221797, 79}, -- Bismuth-Fueled Samophlange
+    {221797, 85}, -- Bismuth-Fueled Samophlange
+    {221797, 91}, -- Bismuth-Fueled Samophlange
+    {221797, 98}, -- Bismuth-Fueled Samophlange
+    {240953, 180}, -- Bold Biographer's Bifocals
+    {240953, 186}, -- Bold Biographer's Bifocals
+    {240953, 192}, -- Bold Biographer's Bifocals
+    {240953, 199}, -- Bold Biographer's Bifocals
+    {240953, 206}, -- Bold Biographer's Bifocals
+    {256653, 0}, -- Pattern: Ranger-General's Grips
+    {258499, 0}, -- Technique: Restful Bronze Bench
+    {267395, 0}, -- Technique: Homely Wall Shelves
+    {30308, 0}, -- Pattern: Hurricane Boots
+    {86283, 0}, -- Pattern: Raiment of Blood and Bone
+    {94573, 0}, -- Direhorn Runt
 }
 
 ArbitrageCache = {
