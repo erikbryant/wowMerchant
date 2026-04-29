4d56b3a2820b532b35256bc68be95c07d9824658
Erik Bryant <erikbryantology@gmail.com>
2026-04-29 08:11:12 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,21 +1,17 @@
 local ItemIDs = {
-    {14513, 0}, -- Pattern: Robe of the Archmage
     {221797, 105}, -- Bismuth-Fueled Samophlange
     {221797, 79}, -- Bismuth-Fueled Samophlange
     {221797, 85}, -- Bismuth-Fueled Samophlange
     {221797, 91}, -- Bismuth-Fueled Samophlange
     {221797, 98}, -- Bismuth-Fueled Samophlange
-    {240953, 180}, -- Bold Biographer's Bifocals
-    {240953, 186}, -- Bold Biographer's Bifocals
-    {240953, 192}, -- Bold Biographer's Bifocals
-    {240953, 199}, -- Bold Biographer's Bifocals
-    {240953, 206}, -- Bold Biographer's Bifocals
+    {223060, 0}, -- Technique: Patient Alchemist's Mixing Rod
+    {223085, 0}, -- Design: Fractured Gemstone Locket
     {256653, 0}, -- Pattern: Ranger-General's Grips
-    {258499, 0}, -- Technique: Restful Bronze Bench
+    {258134, 0}, -- Pattern: Voidstrider Saddlebag
     {267395, 0}, -- Technique: Homely Wall Shelves
-    {30308, 0}, -- Pattern: Hurricane Boots
-    {86283, 0}, -- Pattern: Raiment of Blood and Bone
-    {94573, 0}, -- Direhorn Runt
+    {30283, 0}, -- Pattern: Boots of the Long Road
+    {30323, 0}, -- Plans: Boots of the Protector
+    {86380, 0}, -- Pattern: Imperial Silk Gloves
 }
 
 ArbitrageCache = {
