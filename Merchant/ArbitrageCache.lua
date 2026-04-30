1cde5364685b10576991f99b79fc957f3edc11c7
Erik Bryant <erikbryantology@gmail.com>
2026-04-30 07:51:37 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,17 +1,28 @@
 local ItemIDs = {
-    {221797, 105}, -- Bismuth-Fueled Samophlange
-    {221797, 79}, -- Bismuth-Fueled Samophlange
-    {221797, 85}, -- Bismuth-Fueled Samophlange
-    {221797, 91}, -- Bismuth-Fueled Samophlange
-    {221797, 98}, -- Bismuth-Fueled Samophlange
+    {18257, 0}, -- Recipe: Major Rejuvenation Potion
+    {191237, 70}, -- Draconium Blacksmith's Hammer
+    {191237, 71}, -- Draconium Blacksmith's Hammer
+    {191237, 72}, -- Draconium Blacksmith's Hammer
+    {191237, 73}, -- Draconium Blacksmith's Hammer
+    {191237, 74}, -- Draconium Blacksmith's Hammer
     {223060, 0}, -- Technique: Patient Alchemist's Mixing Rod
     {223085, 0}, -- Design: Fractured Gemstone Locket
+    {223101, 0}, -- Pattern: Reinforced Setae Flyers
+    {225465, 0}, -- Plans: Algari Competitor's Plate Waistguard
+    {244620, 0}, -- Chemist's Cap
+    {244627, 180}, -- Apprentice Smith's Apron
+    {244627, 186}, -- Apprentice Smith's Apron
+    {244627, 192}, -- Apprentice Smith's Apron
+    {244627, 199}, -- Apprentice Smith's Apron
+    {244627, 206}, -- Apprentice Smith's Apron
     {256653, 0}, -- Pattern: Ranger-General's Grips
     {258134, 0}, -- Pattern: Voidstrider Saddlebag
+    {258499, 0}, -- Technique: Restful Bronze Bench
     {267395, 0}, -- Technique: Homely Wall Shelves
-    {30283, 0}, -- Pattern: Boots of the Long Road
     {30323, 0}, -- Plans: Boots of the Protector
-    {86380, 0}, -- Pattern: Imperial Silk Gloves
+    {4499, 0}, -- Huge Brown Sack
+    {48120, 0}, -- Obsidian Hatchling
+    {86281, 0}, -- Pattern: Nightfire Robe
 }
 
 ArbitrageCache = {
