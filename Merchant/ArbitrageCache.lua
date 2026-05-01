e1f1b6a6d2354bab3454d6716c8d3d77a3a64f6f
Erik Bryant <erikbryantology@gmail.com>
2026-04-30 20:36:51 -0700

    Update skip list

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,26 +1,25 @@
 local ItemIDs = {
-    {18257, 0}, -- Recipe: Major Rejuvenation Potion
-    {191237, 70}, -- Draconium Blacksmith's Hammer
-    {191237, 71}, -- Draconium Blacksmith's Hammer
-    {191237, 72}, -- Draconium Blacksmith's Hammer
-    {191237, 73}, -- Draconium Blacksmith's Hammer
-    {191237, 74}, -- Draconium Blacksmith's Hammer
+    {193882, 0}, -- Pattern: Acidic Hailstone Treads
+    {21296, 0}, -- Book of Rejuvenation
     {223060, 0}, -- Technique: Patient Alchemist's Mixing Rod
     {223085, 0}, -- Design: Fractured Gemstone Locket
-    {223101, 0}, -- Pattern: Reinforced Setae Flyers
+    {224424, 0}, -- Pattern: Artisan Chef's Hat
     {225465, 0}, -- Plans: Algari Competitor's Plate Waistguard
-    {244620, 0}, -- Chemist's Cap
+    {244618, 180}, -- Tinker's Handguard
+    {244618, 186}, -- Tinker's Handguard
+    {244618, 192}, -- Tinker's Handguard
+    {244618, 199}, -- Tinker's Handguard
+    {244618, 206}, -- Tinker's Handguard
     {244627, 180}, -- Apprentice Smith's Apron
     {244627, 186}, -- Apprentice Smith's Apron
     {244627, 192}, -- Apprentice Smith's Apron
     {244627, 199}, -- Apprentice Smith's Apron
     {244627, 206}, -- Apprentice Smith's Apron
-    {256653, 0}, -- Pattern: Ranger-General's Grips
-    {258134, 0}, -- Pattern: Voidstrider Saddlebag
+    {256625, 0}, -- Pattern: Hexwoven Strand
+    {256652, 0}, -- Pattern: World Tender's Trunkplate
     {258499, 0}, -- Technique: Restful Bronze Bench
     {267395, 0}, -- Technique: Homely Wall Shelves
-    {30323, 0}, -- Plans: Boots of the Protector
-    {4499, 0}, -- Huge Brown Sack
+    {30307, 0}, -- Pattern: Boots of the Crimson Hawk
     {48120, 0}, -- Obsidian Hatchling
     {86281, 0}, -- Pattern: Nightfire Robe
 }
