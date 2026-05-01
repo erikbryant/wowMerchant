eee014ff7862c2dd971f86149ca00ed8bf93e810
Erik Bryant <erikbryantology@gmail.com>
2026-05-01 09:55:25 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,27 +1,16 @@
 local ItemIDs = {
-    {193882, 0}, -- Pattern: Acidic Hailstone Treads
-    {21296, 0}, -- Book of Rejuvenation
-    {223060, 0}, -- Technique: Patient Alchemist's Mixing Rod
+    {136972, 0}, -- Tome of Hex: Cockroach
     {223085, 0}, -- Design: Fractured Gemstone Locket
-    {224424, 0}, -- Pattern: Artisan Chef's Hat
-    {225465, 0}, -- Plans: Algari Competitor's Plate Waistguard
-    {244618, 180}, -- Tinker's Handguard
-    {244618, 186}, -- Tinker's Handguard
-    {244618, 192}, -- Tinker's Handguard
-    {244618, 199}, -- Tinker's Handguard
-    {244618, 206}, -- Tinker's Handguard
+    {223101, 0}, -- Pattern: Reinforced Setae Flyers
     {244627, 180}, -- Apprentice Smith's Apron
     {244627, 186}, -- Apprentice Smith's Apron
     {244627, 192}, -- Apprentice Smith's Apron
     {244627, 199}, -- Apprentice Smith's Apron
     {244627, 206}, -- Apprentice Smith's Apron
-    {256625, 0}, -- Pattern: Hexwoven Strand
-    {256652, 0}, -- Pattern: World Tender's Trunkplate
     {258499, 0}, -- Technique: Restful Bronze Bench
     {267395, 0}, -- Technique: Homely Wall Shelves
-    {30307, 0}, -- Pattern: Boots of the Crimson Hawk
+    {32749, 0}, -- Pattern: Shoulders of Lightning Reflexes
     {48120, 0}, -- Obsidian Hatchling
-    {86281, 0}, -- Pattern: Nightfire Robe
 }
 
 ArbitrageCache = {
