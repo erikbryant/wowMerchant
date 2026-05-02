1650f54f6cde99e74f6397fbf76a3ecccf3d36c5
Erik Bryant <erikbryantology@gmail.com>
2026-05-01 17:35:51 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,7 +1,13 @@
 local ItemIDs = {
+    {118427, 0}, -- Autographed Hearthstone Card
     {136972, 0}, -- Tome of Hex: Cockroach
+    {191597, 0}, -- Recipe: Potion Absorption Inhibitor
     {223085, 0}, -- Design: Fractured Gemstone Locket
-    {223101, 0}, -- Pattern: Reinforced Setae Flyers
+    {240954, 180}, -- Fantastic Font Focuser
+    {240954, 186}, -- Fantastic Font Focuser
+    {240954, 192}, -- Fantastic Font Focuser
+    {240954, 199}, -- Fantastic Font Focuser
+    {240954, 206}, -- Fantastic Font Focuser
     {244627, 180}, -- Apprentice Smith's Apron
     {244627, 186}, -- Apprentice Smith's Apron
     {244627, 192}, -- Apprentice Smith's Apron
@@ -9,6 +15,7 @@ local ItemIDs = {
     {244627, 206}, -- Apprentice Smith's Apron
     {258499, 0}, -- Technique: Restful Bronze Bench
     {267395, 0}, -- Technique: Homely Wall Shelves
+    {30307, 0}, -- Pattern: Boots of the Crimson Hawk
     {32749, 0}, -- Pattern: Shoulders of Lightning Reflexes
     {48120, 0}, -- Obsidian Hatchling
 }
