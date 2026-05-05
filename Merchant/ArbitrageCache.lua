eadecfeddf694bfeb183fc14c78528a3e5c6e734
Erik Bryant <erikbryantology@gmail.com>
2026-05-05 14:30:58 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,27 +1,20 @@
 local ItemIDs = {
+    {191242, 70}, -- Draconium Pickaxe
+    {191242, 71}, -- Draconium Pickaxe
+    {191242, 72}, -- Draconium Pickaxe
+    {191242, 73}, -- Draconium Pickaxe
+    {191242, 74}, -- Draconium Pickaxe
     {191580, 0}, -- Recipe: Transmute: Awakened Earth
     {222577, 0}, -- Burnt Rolling Pin
-    {223085, 0}, -- Design: Fractured Gemstone Locket
     {223096, 0}, -- Pattern: Roiling Thunderstrike Talons
-    {223101, 0}, -- Pattern: Reinforced Setae Flyers
-    {240953, 180}, -- Bold Biographer's Bifocals
-    {240953, 186}, -- Bold Biographer's Bifocals
-    {240953, 192}, -- Bold Biographer's Bifocals
-    {240953, 199}, -- Bold Biographer's Bifocals
-    {240953, 206}, -- Bold Biographer's Bifocals
+    {244709, 180}, -- Junker's Junk Visor
+    {244709, 186}, -- Junker's Junk Visor
+    {244709, 192}, -- Junker's Junk Visor
+    {244709, 206}, -- Junker's Junk Visor
     {258134, 0}, -- Pattern: Voidstrider Saddlebag
     {258499, 0}, -- Technique: Restful Bronze Bench
     {267395, 0}, -- Technique: Homely Wall Shelves
-    {30324, 0}, -- Plans: Red Havoc Boots
-    {45088, 0}, -- Plans: Belt of the Titans
-    {45089, 0}, -- Plans: Battlelord's Plate Boots
-    {45090, 0}, -- Plans: Plate Girdle of Righteousness
-    {45092, 0}, -- Plans: Indestructible Plate Girdle
-    {45093, 0}, -- Plans: Spiked Deathdealers
-    {86279, 0}, -- Pattern: Liferuned Leather Gloves
     {86283, 0}, -- Pattern: Raiment of Blood and Bone
-    {87410, 0}, -- Plans: Ornate Battleplate of the Master
-    {94573, 0}, -- Direhorn Runt
 }
 
 ArbitrageCache = {
