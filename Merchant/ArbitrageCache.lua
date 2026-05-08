cdfde71b98a2eed1948fadf47877a532033ded7a
Erik Bryant <erikbryantology@gmail.com>
2026-05-07 21:29:48 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,20 +1,12 @@
 local ItemIDs = {
-    {191242, 70}, -- Draconium Pickaxe
-    {191242, 71}, -- Draconium Pickaxe
-    {191242, 72}, -- Draconium Pickaxe
-    {191242, 73}, -- Draconium Pickaxe
-    {191242, 74}, -- Draconium Pickaxe
-    {191580, 0}, -- Recipe: Transmute: Awakened Earth
     {222577, 0}, -- Burnt Rolling Pin
-    {223096, 0}, -- Pattern: Roiling Thunderstrike Talons
-    {244709, 180}, -- Junker's Junk Visor
-    {244709, 186}, -- Junker's Junk Visor
-    {244709, 192}, -- Junker's Junk Visor
-    {244709, 206}, -- Junker's Junk Visor
-    {258134, 0}, -- Pattern: Voidstrider Saddlebag
-    {258499, 0}, -- Technique: Restful Bronze Bench
-    {267395, 0}, -- Technique: Homely Wall Shelves
-    {86283, 0}, -- Pattern: Raiment of Blood and Bone
+    {223060, 0}, -- Technique: Patient Alchemist's Mixing Rod
+    {223085, 0}, -- Design: Fractured Gemstone Locket
+    {240953, 180}, -- Bold Biographer's Bifocals
+    {240953, 186}, -- Bold Biographer's Bifocals
+    {240953, 192}, -- Bold Biographer's Bifocals
+    {240953, 199}, -- Bold Biographer's Bifocals
+    {240953, 206}, -- Bold Biographer's Bifocals
 }
 
 ArbitrageCache = {
