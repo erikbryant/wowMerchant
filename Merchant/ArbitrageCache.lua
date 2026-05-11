5fe54a06bec523fa0f92f8aa7069817dd0d1e17d
Erik Bryant <erikbryantology@gmail.com>
2026-05-11 10:06:43 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,11 +1,28 @@
 local ItemIDs = {
-    {191580, 0}, -- Recipe: Transmute: Awakened Earth
-    {205129, 0}, -- Technique: Winding Slitherdrake: Curved Chin Horn
     {223085, 0}, -- Design: Fractured Gemstone Locket
+    {223087, 0}, -- Design: Culminating Blasphemite
+    {238012, 180}, -- Thalassian Leatherworker's Knife
+    {238012, 186}, -- Thalassian Leatherworker's Knife
+    {238012, 192}, -- Thalassian Leatherworker's Knife
+    {238012, 199}, -- Thalassian Leatherworker's Knife
+    {238012, 206}, -- Thalassian Leatherworker's Knife
+    {238013, 180}, -- Thalassian Blacksmith's Hammer
+    {238013, 186}, -- Thalassian Blacksmith's Hammer
+    {238013, 192}, -- Thalassian Blacksmith's Hammer
+    {238013, 199}, -- Thalassian Blacksmith's Hammer
+    {238013, 206}, -- Thalassian Blacksmith's Hammer
+    {240953, 180}, -- Bold Biographer's Bifocals
+    {240953, 186}, -- Bold Biographer's Bifocals
+    {240953, 192}, -- Bold Biographer's Bifocals
+    {240953, 199}, -- Bold Biographer's Bifocals
+    {240953, 206}, -- Bold Biographer's Bifocals
     {258499, 0}, -- Technique: Restful Bronze Bench
     {267395, 0}, -- Technique: Homely Wall Shelves
-    {60335, 0}, -- Thick Hide Pack
-    {86280, 0}, -- Pattern: Murderer's Gloves
+    {29363, 0}, -- Mana Wyrmling
+    {30323, 0}, -- Plans: Boots of the Protector
+    {47650, 0}, -- Pattern: Ensorcelled Nerubian Breastplate
+    {86272, 0}, -- Pattern: Fists of Lightning
+    {86284, 0}, -- Pattern: Raven Lord's Gloves
 }
 
 ArbitrageCache = {
