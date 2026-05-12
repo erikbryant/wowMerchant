ae0927c918a81f770423472eb2ac74b88c4f4e1a
Erik Bryant <erikbryantology@gmail.com>
2026-05-12 09:55:02 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,9 +1,20 @@
 local ItemIDs = {
+    {223060, 0}, -- Technique: Patient Alchemist's Mixing Rod
     {223085, 0}, -- Design: Fractured Gemstone Locket
+    {223101, 0}, -- Pattern: Reinforced Setae Flyers
+    {240953, 180}, -- Bold Biographer's Bifocals
+    {240953, 186}, -- Bold Biographer's Bifocals
+    {240953, 192}, -- Bold Biographer's Bifocals
+    {240953, 199}, -- Bold Biographer's Bifocals
+    {240953, 206}, -- Bold Biographer's Bifocals
+    {244175, 180}, -- Runed Refulgent Copper Rod
+    {244175, 186}, -- Runed Refulgent Copper Rod
+    {244175, 192}, -- Runed Refulgent Copper Rod
+    {244175, 199}, -- Runed Refulgent Copper Rod
+    {244175, 206}, -- Runed Refulgent Copper Rod
     {267395, 0}, -- Technique: Homely Wall Shelves
-    {29363, 0}, -- Mana Wyrmling
-    {86272, 0}, -- Pattern: Fists of Lightning
-    {86284, 0}, -- Pattern: Raven Lord's Gloves
+    {47657, 0}, -- Pattern: Royal Moonshroud Robe
+    {94573, 0}, -- Direhorn Runt
 }
 
 ArbitrageCache = {
