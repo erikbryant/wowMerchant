c999f3def5687f242c85aed44e2e32a58192e148
Erik Bryant <erikbryantology@gmail.com>
2026-05-15 10:50:19 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,8 +1,14 @@
 local ItemIDs = {
-    {18257, 0}, -- Recipe: Major Rejuvenation Potion
+    {108745, 0}, -- Personal Hologram
+    {223060, 0}, -- Technique: Patient Alchemist's Mixing Rod
     {223085, 0}, -- Design: Fractured Gemstone Locket
+    {223101, 0}, -- Pattern: Reinforced Setae Flyers
+    {256653, 0}, -- Pattern: Ranger-General's Grips
+    {256716, 0}, -- Design: Prismatic Focusing Iris
+    {256761, 0}, -- Formula: Enchant Shoulders - Voidtouched
     {267395, 0}, -- Technique: Homely Wall Shelves
-    {68788, 0}, -- Formula: Enchant Bracer - Major Strength
+    {30306, 0}, -- Pattern: Boots of Utter Darkness
+    {3914, 0}, -- Journeyman's Backpack
 }
 
 ArbitrageCache = {
