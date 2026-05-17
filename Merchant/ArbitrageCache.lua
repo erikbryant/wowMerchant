c302b1f016ab6ecf2a87ab8a106c6ff7db40fc8d
Erik Bryant <erikbryantology@gmail.com>
2026-05-17 12:49:12 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,14 +1,20 @@
 local ItemIDs = {
-    {108745, 0}, -- Personal Hologram
+    {118427, 0}, -- Autographed Hearthstone Card
+    {191237, 70}, -- Draconium Blacksmith's Hammer
+    {191237, 71}, -- Draconium Blacksmith's Hammer
+    {191237, 72}, -- Draconium Blacksmith's Hammer
+    {191237, 73}, -- Draconium Blacksmith's Hammer
+    {191237, 74}, -- Draconium Blacksmith's Hammer
+    {191578, 0}, -- Recipe: Transmute: Awakened Fire
+    {191580, 0}, -- Recipe: Transmute: Awakened Earth
     {223060, 0}, -- Technique: Patient Alchemist's Mixing Rod
     {223085, 0}, -- Design: Fractured Gemstone Locket
     {223101, 0}, -- Pattern: Reinforced Setae Flyers
-    {256653, 0}, -- Pattern: Ranger-General's Grips
-    {256716, 0}, -- Design: Prismatic Focusing Iris
     {256761, 0}, -- Formula: Enchant Shoulders - Voidtouched
     {267395, 0}, -- Technique: Homely Wall Shelves
+    {29363, 0}, -- Mana Wyrmling
     {30306, 0}, -- Pattern: Boots of Utter Darkness
-    {3914, 0}, -- Journeyman's Backpack
+    {86238, 0}, -- Pattern: Chestguard of Nemeses
 }
 
 ArbitrageCache = {
