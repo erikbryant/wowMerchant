c262111f7227e36b92ef5b7e514d084c5cf11626
Erik Bryant <erikbryantology@gmail.com>
2026-05-31 13:13:15 -0700

    Update price cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,19 +1,10 @@
 local ItemIDs = {
-    {223060, 0}, -- Technique: Patient Alchemist's Mixing Rod
+    {138004, 0}, -- Pattern: Imbued Silkweave Flourish
+    {191578, 0}, -- Recipe: Transmute: Awakened Fire
     {223085, 0}, -- Design: Fractured Gemstone Locket
-    {240953, 180}, -- Bold Biographer's Bifocals
-    {240953, 186}, -- Bold Biographer's Bifocals
-    {240953, 192}, -- Bold Biographer's Bifocals
-    {240953, 199}, -- Bold Biographer's Bifocals
-    {240953, 206}, -- Bold Biographer's Bifocals
-    {245775, 180}, -- Hobbyist Scribe's Quill
-    {245775, 186}, -- Hobbyist Scribe's Quill
-    {245775, 192}, -- Hobbyist Scribe's Quill
-    {245775, 199}, -- Hobbyist Scribe's Quill
-    {245775, 206}, -- Hobbyist Scribe's Quill
+    {258134, 0}, -- Pattern: Voidstrider Saddlebag
     {267395, 0}, -- Technique: Homely Wall Shelves
-    {44822, 0}, -- Albino Snake
-    {86380, 0}, -- Pattern: Imperial Silk Gloves
+    {87413, 0}, -- Plans: Gauntlets of Unbound Devotion
 }
 
 ArbitrageCache = {
