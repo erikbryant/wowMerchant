82b30ab76a617aeb808705a9866365ab2ef983fa
Erik Bryant <erikbryantology@gmail.com>
2026-06-01 15:03:13 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,10 +1,11 @@
 local ItemIDs = {
-    {138004, 0}, -- Pattern: Imbued Silkweave Flourish
+    {12704, 0}, -- Plans: Thorium Leggings
+    {12833, 0}, -- Plans: Hammer of the Titans
     {191578, 0}, -- Recipe: Transmute: Awakened Fire
     {223085, 0}, -- Design: Fractured Gemstone Locket
-    {258134, 0}, -- Pattern: Voidstrider Saddlebag
+    {263516, 0}, -- Cosmic Ritual Bowl
     {267395, 0}, -- Technique: Homely Wall Shelves
-    {87413, 0}, -- Plans: Gauntlets of Unbound Devotion
+    {30307, 0}, -- Pattern: Boots of the Crimson Hawk
 }
 
 ArbitrageCache = {
