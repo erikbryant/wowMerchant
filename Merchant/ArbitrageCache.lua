fa891bb69dc88e2556357664f7341b6816deaf20
Erik Bryant <erikbryantology@gmail.com>
2026-06-09 13:18:40 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,11 +1,15 @@
 local ItemIDs = {
-    {12704, 0}, -- Plans: Thorium Leggings
-    {12833, 0}, -- Plans: Hammer of the Titans
     {191578, 0}, -- Recipe: Transmute: Awakened Fire
-    {223085, 0}, -- Design: Fractured Gemstone Locket
+    {191580, 0}, -- Recipe: Transmute: Awakened Earth
+    {225472, 0}, -- Schematic: Algari Competitor's Leather Bracers
+    {255680, 0}, -- Forgotten Talon
+    {256625, 0}, -- Pattern: Hexwoven Strand
     {263516, 0}, -- Cosmic Ritual Bowl
     {267395, 0}, -- Technique: Homely Wall Shelves
     {30307, 0}, -- Pattern: Boots of the Crimson Hawk
+    {47657, 0}, -- Pattern: Royal Moonshroud Robe
+    {72014, 0}, -- Plans: Soul Redeemer Bracers
+    {86283, 0}, -- Pattern: Raiment of Blood and Bone
 }
 
 ArbitrageCache = {
