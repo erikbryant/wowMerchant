21d91dc32f13bdc5ddc1ddf30fbf060fc6cc9473
Erik Bryant <erikbryantology@gmail.com>
2026-06-10 12:10:15 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,14 +1,9 @@
 local ItemIDs = {
-    {191578, 0}, -- Recipe: Transmute: Awakened Fire
     {191580, 0}, -- Recipe: Transmute: Awakened Earth
-    {225472, 0}, -- Schematic: Algari Competitor's Leather Bracers
-    {255680, 0}, -- Forgotten Talon
-    {256625, 0}, -- Pattern: Hexwoven Strand
-    {263516, 0}, -- Cosmic Ritual Bowl
+    {194266, 0}, -- Pattern: Bronzed Grip Wrappings
     {267395, 0}, -- Technique: Homely Wall Shelves
     {30307, 0}, -- Pattern: Boots of the Crimson Hawk
     {47657, 0}, -- Pattern: Royal Moonshroud Robe
-    {72014, 0}, -- Plans: Soul Redeemer Bracers
     {86283, 0}, -- Pattern: Raiment of Blood and Bone
 }
 
