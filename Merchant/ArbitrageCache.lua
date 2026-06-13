7c73fd324abe420b43f8729c1df9c5f56ab1bafd
Erik Bryant <erikbryantology@gmail.com>
2026-06-13 12:37:56 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,6 +1,4 @@
 local ItemIDs = {
-    {198715, 71}, -- Runed Draconium Rod
-    {198715, 74}, -- Runed Draconium Rod
     {223085, 0}, -- Design: Fractured Gemstone Locket
     {240953, 180}, -- Bold Biographer's Bifocals
     {240953, 186}, -- Bold Biographer's Bifocals
@@ -13,9 +11,8 @@ local ItemIDs = {
     {240954, 199}, -- Fantastic Font Focuser
     {240954, 206}, -- Fantastic Font Focuser
     {24298, 0}, -- Pattern: Blackstrike Bracers
-    {256625, 0}, -- Pattern: Hexwoven Strand
     {256716, 0}, -- Design: Prismatic Focusing Iris
-    {258134, 0}, -- Pattern: Voidstrider Saddlebag
+    {94573, 0}, -- Direhorn Runt
 }
 
 ArbitrageCache = {
