a7f8de19002616ff1d26b7b9ad11342910210ba0
Erik Bryant <erikbryantology@gmail.com>
2026-06-20 07:04:51 -0700

    Update cache

--- Merchant/ArbitrageCache.lua
+++ Merchant/ArbitrageCache.lua
@@ -1,18 +1,13 @@
 local ItemIDs = {
     {223085, 0}, -- Design: Fractured Gemstone Locket
+    {223101, 0}, -- Pattern: Reinforced Setae Flyers
     {240953, 180}, -- Bold Biographer's Bifocals
     {240953, 186}, -- Bold Biographer's Bifocals
     {240953, 192}, -- Bold Biographer's Bifocals
     {240953, 199}, -- Bold Biographer's Bifocals
     {240953, 206}, -- Bold Biographer's Bifocals
-    {240954, 180}, -- Fantastic Font Focuser
-    {240954, 186}, -- Fantastic Font Focuser
-    {240954, 192}, -- Fantastic Font Focuser
-    {240954, 199}, -- Fantastic Font Focuser
-    {240954, 206}, -- Fantastic Font Focuser
-    {24298, 0}, -- Pattern: Blackstrike Bracers
-    {256716, 0}, -- Design: Prismatic Focusing Iris
-    {94573, 0}, -- Direhorn Runt
+    {258499, 0}, -- Technique: Restful Bronze Bench
+    {30748, 0}, -- Enchanter's Satchel
 }
 
 ArbitrageCache = {
